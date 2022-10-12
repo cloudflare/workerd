784c76e436b53e7a1b4e1eb40471fff8132fd819
Frederik Baetens <fbaetens@cloudflare.com>
2022-10-12 22:00:12 +0100

    move multipart implementation into r2-multipart.c++

--- src/workerd/api/r2-multipart.h
+++ src/workerd/api/r2-multipart.h
@@ -21,24 +21,12 @@ class R2MultipartUpload: public jsg::Object {
       jsg::Lock& js,
       int partNumber,
       R2PutValue value,
-      const jsg::TypeHandler<jsg::Ref<R2Error>>& errorType) {
-        return bucket.get()->uploadPart(
-          js, kj::str(key), kj::str(uploadId), partNumber, kj::mv(value), errorType
-        );
-    }
-    jsg::Promise<void> abort(jsg::Lock& js, const jsg::TypeHandler<jsg::Ref<R2Error>>& errorType) {
-      return bucket.get()->abortMultipartUpload(
-        js, kj::str(key), kj::str(uploadId), errorType
-      );
-    }
+      const jsg::TypeHandler<jsg::Ref<R2Error>>& errorType);
+    jsg::Promise<void> abort(jsg::Lock& js, const jsg::TypeHandler<jsg::Ref<R2Error>>& errorType);
     jsg::Promise<jsg::Ref<R2Bucket::HeadResult>> complete(
       jsg::Lock& js,
       kj::Array<R2Bucket::UploadedPart> uploadedParts,
-      const jsg::TypeHandler<jsg::Ref<R2Error>>& errorType) {
-        return bucket.get()->completeMultipartUpload(
-          js, kj::str(key), kj::str(uploadId), kj::mv(uploadedParts), errorType
-        );
-    }
+      const jsg::TypeHandler<jsg::Ref<R2Error>>& errorType);
 
     JSG_RESOURCE_TYPE(R2MultipartUpload) {
       JSG_LAZY_READONLY_INSTANCE_PROPERTY(key, getKey);
