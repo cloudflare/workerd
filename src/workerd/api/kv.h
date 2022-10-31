d60536564c067f540178802f8107c04a0220fc3d
Harris Hancock <harris@cloudflare.com>
2022-10-31 16:33:33 +0000

    Revert "Add an ability to inject an arbitrary set of headers to KV requests"
    This reverts commit 8a6fdac39c4beef86e8e18612315edd6b89560ba.

--- src/workerd/api/kv.h
+++ src/workerd/api/kv.h
@@ -13,16 +13,9 @@ class KvNamespace: public jsg::Object {
   // A capability to a KV namespace.
 
 public:
-  struct AdditionalHeader {
-    kj::String name;
-    kj::String value;
-  };
-
-  explicit KvNamespace(kj::Array<AdditionalHeader> additionalHeaders, uint subrequestChannel)
-      : additionalHeaders(kj::mv(additionalHeaders)), subrequestChannel(subrequestChannel) {}
+  explicit KvNamespace(uint subrequestChannel): subrequestChannel(subrequestChannel) {}
   // `subrequestChannel` is what to pass to IoContext::getHttpClient() to get an HttpClient
   // representing this namespace.
-  // `additionalHeaders` is what gets appended to every outbound request.
 
   struct GetOptions {
     jsg::Optional<kj::String> type;
@@ -97,7 +90,6 @@ public:
   }
 
 private:
-  kj::Array<AdditionalHeader> additionalHeaders;
   uint subrequestChannel;
 };
 
