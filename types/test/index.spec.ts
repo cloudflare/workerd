c5ad3b61af46e399ba8a1c8e788c496ca33b810b
Samuel Macleod <smacleod@cloudflare.com>
2022-12-01 12:46:29 +0000

    Fix tests (include copyright header and the right path)

--- types/test/index.spec.ts
+++ types/test/index.spec.ts
@@ -114,7 +114,7 @@ test("main: generates types", async () => {
   const inputDir = path.join(tmpPath, "capnp");
   await fs.mkdir(inputDir);
   const inputPath = path.join(inputDir, "types.api.capnp.bin");
-  const outputPath = path.join(definitionsDir, "types", "api.d.ts");
+  const outputPath = path.join(definitionsDir, "types", "index.d.ts");
 
   await fs.writeFile(inputPath, new Uint8Array(message.toArrayBuffer()));
 
@@ -122,7 +122,21 @@ test("main: generates types", async () => {
   let output = await fs.readFile(outputPath, "utf8");
   assert.strictEqual(
     output,
-    `/* eslint-disable */
+    `/*! *****************************************************************************
+Copyright (c) Cloudflare. All rights reserved.
+Copyright (c) Microsoft Corporation. All rights reserved.
+
+Licensed under the Apache License, Version 2.0 (the "License"); you may not use
+this file except in compliance with the License. You may obtain a copy of the
+License at http://www.apache.org/licenses/LICENSE-2.0
+THIS CODE IS PROVIDED ON AN *AS IS* BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+KIND, EITHER EXPRESS OR IMPLIED, INCLUDING WITHOUT LIMITATION ANY IMPLIED
+WARRANTIES OR CONDITIONS OF TITLE, FITNESS FOR A PARTICULAR PURPOSE,
+MERCHANTABLITY OR NON-INFRINGEMENT.
+See the Apache Version 2.0 License for specific language governing permissions
+and limitations under the License.
+***************************************************************************** */
+/* eslint-disable */
 // noinspection JSUnusedGlobalSymbols
 /** An event which takes place in the DOM. */
 declare interface Event {
@@ -153,7 +167,21 @@ declare const prop: Promise<number>;
   output = await fs.readFile(outputPath, "utf8");
   assert.strictEqual(
     output,
-    `/* eslint-disable */
+    `/*! *****************************************************************************
+Copyright (c) Cloudflare. All rights reserved.
+Copyright (c) Microsoft Corporation. All rights reserved.
+
+Licensed under the Apache License, Version 2.0 (the "License"); you may not use
+this file except in compliance with the License. You may obtain a copy of the
+License at http://www.apache.org/licenses/LICENSE-2.0
+THIS CODE IS PROVIDED ON AN *AS IS* BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+KIND, EITHER EXPRESS OR IMPLIED, INCLUDING WITHOUT LIMITATION ANY IMPLIED
+WARRANTIES OR CONDITIONS OF TITLE, FITNESS FOR A PARTICULAR PURPOSE,
+MERCHANTABLITY OR NON-INFRINGEMENT.
+See the Apache Version 2.0 License for specific language governing permissions
+and limitations under the License.
+***************************************************************************** */
+/* eslint-disable */
 // noinspection JSUnusedGlobalSymbols
 /** An event which takes place in the DOM. */
 declare interface Event {}
