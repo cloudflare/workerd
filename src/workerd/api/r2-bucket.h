8231c992c868af5c1efc17149eb20a8f721d0e53
Frederik Baetens <fbaetens@cloudflare.com>
2022-12-07 21:10:16 +0000

    add conditional JSG type for R2 put

--- src/workerd/api/r2-bucket.h
+++ src/workerd/api/r2-bucket.h
@@ -338,6 +338,10 @@ public:
       get(key: string, options?: R2GetOptions): Promise<R2ObjectBody | null>;
 
       put(key: string, value: ReadableStream | ArrayBuffer | ArrayBufferView | string | null | Blob, options?: R2PutOptions): Promise<R2Object>;
+      put(
+        key: string, value: ReadableStream | ArrayBuffer | ArrayBufferView | string | null | Blob,
+        options?: R2PutOptions & { onlyIf: R2BucketConditional | Headers }
+      ): Promise<R2Object | null>;
     });
     // Exclude `R2Object` from `get` return type if `onlyIf` not specified, and exclude `null` from `put` return type
 
