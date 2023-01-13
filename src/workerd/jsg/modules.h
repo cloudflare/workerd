2b135c3ab09cfb098f5940f6bec15e09496c12c8
Mike Aizatsky <maizatskyi@cloudflare.com>
2023-01-13 12:19:25 -0800

    Revert "Revert "node compatibility typescript infrastructure""
    This reverts commit e88bae5f3817d5d40c06a7b988fd284be1a96b98.

--- src/workerd/jsg/modules.h
+++ src/workerd/jsg/modules.h
@@ -7,6 +7,7 @@
 #include "jsg.h"
 #include <kj/filesystem.h>
 #include <kj/map.h>
+#include <workerd/jsg/modules.capnp.h>
 
 namespace workerd::jsg {
 
@@ -236,6 +237,14 @@ public:
 
   void add(kj::Path& specifier, ModuleInfo&& info) {
     entries.insert(Entry(specifier, Type::BUNDLE, kj::fwd<ModuleInfo>(info)));
+  }
+
+  void addBuiltinBundle(Bundle::Reader bundle) {
+    for (auto module: bundle.getModules()) {
+      // TODO: asChars() might be wrong for wide characters
+      addBuiltinModule(module.getName(), module.getSrc().asChars(),
+          module.getInternal() ? Type::INTERNAL : Type::BUILTIN);
+    }
   }
 
   void addBuiltinModule(kj::StringPtr specifier,
