c044383cad0931f02f9f0be1a243f71c824693fc
Kenton Varda <kenton@cloudflare.com>
2023-01-20 21:28:42 -0600

    Add SqliteKv utility class, implementing KV storage on SQLite.
    This is so trivial that it almost doesn't need to be a separate class... but it's nice to have a unit test verifying the SQL queries directly.

---
+++ src/workerd/util/sqlite-kv.c++
@@ -0,0 +1,92 @@
+// Copyright (c) 2023 Cloudflare, Inc.
+// Licensed under the Apache 2.0 license found in the LICENSE file or at:
+//     https://opensource.org/licenses/Apache-2.0
+
+#include "sqlite-kv.h"
+
+namespace workerd {
+
+SqliteKv::SqliteKv(Sqlite& db, bool): db(db) {}
+
+Sqlite& SqliteKv::ensureInitialized(Sqlite& db) {
+  // TODO(sqlite): Do this automatically at a lower layer?
+  db.run("PRAGMA journal_mode=WAL;");
+
+  db.run(R"(
+    CREATE TABLE IF NOT EXISTS _cf_KV (
+      key TEXT PRIMARY KEY,
+      value BLOB
+    ) WITHOUT ROWID;
+  )");
+
+  return db;
+}
+
+bool SqliteKv::get(KeyPtr key, kj::FunctionParam<void(ValuePtr)> callback) {
+  auto query = stmtGet.run(key);
+
+  if (query.isDone()) {
+    return false;
+  } else {
+    callback(query.getBlob(0));
+    return true;
+  }
+}
+
+uint SqliteKv::list(KeyPtr begin, kj::Maybe<KeyPtr> end, kj::Maybe<uint> limit, Order order,
+                    kj::FunctionParam<void(KeyPtr, ValuePtr)> callback) {
+  auto iterate = [&](Sqlite::Query&& query) {
+    size_t count = 0;
+    while (!query.isDone()) {
+      callback(query.getText(0), query.getBlob(1));
+      query.nextRow();
+      ++count;
+    }
+    return count;
+  };
+
+  if (order == Order::FORWARD) {
+    KJ_IF_MAYBE(e, end) {
+      KJ_IF_MAYBE(l, limit) {
+        return iterate(stmtListEndLimit.run(begin, *e, (int64_t)*l));
+      } else {
+        return iterate(stmtListEnd.run(begin, *e));
+      }
+    } else {
+      KJ_IF_MAYBE(l, limit) {
+        return iterate(stmtListLimit.run(begin, (int64_t)*l));
+      } else {
+        return iterate(stmtList.run(begin));
+      }
+    }
+  } else {
+    KJ_IF_MAYBE(e, end) {
+      KJ_IF_MAYBE(l, limit) {
+        return iterate(stmtListEndLimitReverse.run(begin, *e, (int64_t)*l));
+      } else {
+        return iterate(stmtListEndReverse.run(begin, *e));
+      }
+    } else {
+      KJ_IF_MAYBE(l, limit) {
+        return iterate(stmtListLimitReverse.run(begin, (int64_t)*l));
+      } else {
+        return iterate(stmtListReverse.run(begin));
+      }
+    }
+  }
+}
+
+void SqliteKv::put(KeyPtr key, ValuePtr value) {
+  stmtPut.run(key, value);
+}
+
+bool SqliteKv::delete_(KeyPtr key) {
+  auto query = stmtDelete.run(key);
+  return query.changeCount() > 0;
+}
+
+void SqliteKv::deleteAll() {
+  stmtDeleteAll.run();
+}
+
+}  // namespace workerd
---
+++ src/workerd/util/sqlite-kv.h
@@ -0,0 +1,121 @@
+// Copyright (c) 2023 Cloudflare, Inc.
+// Licensed under the Apache 2.0 license found in the LICENSE file or at:
+//     https://opensource.org/licenses/Apache-2.0
+
+#pragma once
+
+#include "sqlite.h"
+
+namespace workerd {
+
+class SqliteKv {
+  // Class which implements KV storage on top of SQLite. This is intended to be used for Durable
+  // Object storage.
+  //
+  // The table is named `_cf_KV`. The naming is designed so that if the application is allowed to
+  // perform direct SQL queries, we can block it from accessing any table prefixed with `_cf_`.
+  // (Ideally this class would allow configuring the table name, but this would require a somewhat
+  // obnoxious about of string allocation.)
+
+public:
+  explicit SqliteKv(Sqlite& db): SqliteKv(ensureInitialized(db), true) {}
+
+  typedef kj::StringPtr KeyPtr;
+  typedef kj::ArrayPtr<const kj::byte> ValuePtr;
+
+  bool get(KeyPtr key, kj::FunctionParam<void(ValuePtr)> callback);
+  // Search for a match for the given key. Calls the callback function with the result, if found.
+  // This is intended to avoid the need to copy the bytes, if the caller would just parse them and
+  // drop them immediately anyway. Returns true if there was a match, false if not.
+
+  enum Order {
+    FORWARD,
+    REVERSE
+  };
+
+  uint list(KeyPtr begin, kj::Maybe<KeyPtr> end, kj::Maybe<uint> limit, Order order,
+            kj::FunctionParam<void(KeyPtr, ValuePtr)> callback);
+  // Search for all knows keys and values in a range, calling the callback for each one seen.
+  // `end` and `limit` can be null to request no constraint be enforced.
+
+  void put(KeyPtr key, ValuePtr value);
+  // Store a value into the table.
+
+  bool delete_(KeyPtr key);
+  // Delete the key and return whether it was matched.
+
+  void deleteAll();
+
+  // TODO(perf): Should we provide multi-get, multi-put, and multi-delete? It's a bit tricky to
+  //   implement them as single SQL queries, while still using prepared statements. The c-array
+  //   extension might help here, though it can only support arrays of NUL-terminated strings, not
+  //   byte blobs or strings containing NUL bytes.
+
+private:
+  Sqlite& db;
+
+  Sqlite::Statement stmtGet = db.prepare(R"(
+    SELECT value FROM _cf_KV WHERE key = ?
+  )");
+  Sqlite::Statement stmtPut = db.prepare(R"(
+    INSERT INTO _cf_KV VALUES(?, ?)
+      ON CONFLICT DO UPDATE SET value = excluded.value;
+  )");
+  Sqlite::Statement stmtDelete = db.prepare(R"(
+    DELETE FROM _cf_KV WHERE key = ?
+  )");
+  Sqlite::Statement stmtList = db.prepare(R"(
+    SELECT * FROM _cf_KV
+    WHERE key >= ?
+    ORDER BY key
+  )");
+  Sqlite::Statement stmtListEnd = db.prepare(R"(
+    SELECT * FROM _cf_KV
+    WHERE key >= ? AND key < ?
+    ORDER BY key
+  )");
+  Sqlite::Statement stmtListLimit = db.prepare(R"(
+    SELECT * FROM _cf_KV
+    WHERE key >= ?
+    ORDER BY key
+    LIMIT ?
+  )");
+  Sqlite::Statement stmtListEndLimit = db.prepare(R"(
+    SELECT * FROM _cf_KV
+    WHERE key >= ? AND key < ?
+    ORDER BY key
+    LIMIT ?
+  )");
+  Sqlite::Statement stmtListReverse = db.prepare(R"(
+    SELECT * FROM _cf_KV
+    WHERE key >= ?
+    ORDER BY key DESC
+  )");
+  Sqlite::Statement stmtListEndReverse = db.prepare(R"(
+    SELECT * FROM _cf_KV
+    WHERE key >= ? AND key < ?
+    ORDER BY key DESC
+  )");
+  Sqlite::Statement stmtListLimitReverse = db.prepare(R"(
+    SELECT * FROM _cf_KV
+    WHERE key >= ?
+    ORDER BY key DESC
+    LIMIT ?
+  )");
+  Sqlite::Statement stmtListEndLimitReverse = db.prepare(R"(
+    SELECT * FROM _cf_KV
+    WHERE key >= ? AND key < ?
+    ORDER BY key DESC
+    LIMIT ?
+  )");
+  Sqlite::Statement stmtDeleteAll = db.prepare(R"(
+    DELETE FROM _cf_KV
+  )");
+
+  Sqlite& ensureInitialized(Sqlite& db);
+  // Make sure the KV table is created, then return the same object.
+
+  SqliteKv(Sqlite& db, bool);
+};
+
+}  // namespace workerd
