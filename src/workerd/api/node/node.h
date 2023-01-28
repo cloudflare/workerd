fa00599e8951db4d0ba1347dd4648bc2326b8272
James M Snell <jsnell@cloudflare.com>
2023-01-27 16:47:56 -0800

    Add workerd:compatibility-flags internal built-in module
    For use only by built-in TypeScript modules, this provides an import
    that gives information on the currently set compatibility flags.
    
    e.g.
    
    ```
    import { default as flags } from 'worker:compatibility-flags';
    console.log(flags.nodeJsCompat); // true|false
    ```
    
    The flags are set as readonly booleans on the export.

--- src/workerd/api/node/node.h
+++ src/workerd/api/node/node.h
@@ -1,21 +1,44 @@
 #pragma once
 
 #include "async-hooks.h"
+#include <workerd/jsg/jsg.h>
 #include <workerd/jsg/modules.h>
+#include <capnp/dynamic.h>
 #include <node/bundle.capnp.h>
 
 namespace workerd::api::node {
 
+class CompatibilityFlags : public jsg::Object {
+  // To be exposed only as an internal module for use by other built-ins.
+  // TODO(later): Consider moving out of node.h when needed for other
+  // built-ins
+public:
+  JSG_RESOURCE_TYPE(CompatibilityFlags, workerd::CompatibilityFlags::Reader flags) {
+    // Not your typical JSG_RESOURCE_TYPE definition.. here we are iterating
+    // through all of the compatibility flags and registering each as read-only
+    // literal values on the instance...
+    auto dynamic = capnp::toDynamic(flags);
+    auto schema = dynamic.getSchema();
+    for (auto field : schema.getFields()) {
+      registry.template registerReadonlyInstanceProperty<bool>(
+          field.getProto().getName(),
+          dynamic.get(field).as<bool>());
+    }
+  }
+};
+
 template <typename TypeWrapper>
 void registerNodeJsCompatModules(
     workerd::jsg::ModuleRegistryImpl<TypeWrapper>& registry, auto featureFlags) {
+  registry.template addBuiltinModule<CompatibilityFlags>("workerd:compatibility-flags",
+      workerd::jsg::ModuleRegistry::Type::INTERNAL);
   registry.template addBuiltinModule<AsyncHooksModule>("node-internal:async_hooks",
       workerd::jsg::ModuleRegistry::Type::INTERNAL);
-
   registry.addBuiltinBundle(NODE_BUNDLE);
 }
 
-#define EW_NODE_ISOLATE_TYPES \
+#define EW_NODE_ISOLATE_TYPES      \
+  api::node::CompatibilityFlags,   \
   EW_NODE_ASYNCHOOKS_ISOLATE_TYPES
 
 }  // namespace workerd::api::node
