1ae2b68dac453aa09d0a44b907a1e510e7c4d0c5
Samuel Macleod <smacleod@cloudflare.com>
2023-02-15 16:58:43 +0000

    Copy out caching

--- build-releases.sh
+++ build-releases.sh
@@ -3,6 +3,8 @@ set -euo pipefail
 
 # Note: this _must_ be run on an Apple Silicon machine, since the macOS ARM build cannot be dockerised due to macOS license restrictions
 
+rm -rf workerd-*
+
 # Get the tag associated with the latest release, to ensure parity between binaries
 TAG_NAME=$(curl -sL https://api.github.com/repos/cloudflare/workerd/releases/latest | jq -r ".tag_name")
 
@@ -13,4 +15,4 @@ npx -p @bazel/bazelisk bazelisk build --disk_cache=./.bazel-cache -c opt //src/w
 
 cp bazel-bin/src/workerd/server/workerd ./workerd-darwin-arm64
 
-docker buildx build --platform linux/arm64 -f Dockerfile.release --target=artifact --output type=local,dest=$(pwd) .
+docker buildx build --platform linux/arm64 -f Dockerfile.release -t workerd:$TAG_NAME --target=artifact --output type=local,dest=$(pwd) .
