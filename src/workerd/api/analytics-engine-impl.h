8983248fc737b741ad6d3874162af35b30e99a86
Harris Hancock <harris@cloudflare.com>
2023-03-28 14:11:10 +0100

    Increase Analytics Engine index byte limit 32 -> 96

--- src/workerd/api/analytics-engine-impl.h
+++ src/workerd/api/analytics-engine-impl.h
@@ -11,7 +11,7 @@ namespace workerd::api {
 using kj::uint;
 
 constexpr uint MAX_INDEXES_LENGTH = 1;
-constexpr size_t MAX_INDEX_SIZE_IN_BYTES = 32;
+constexpr size_t MAX_INDEX_SIZE_IN_BYTES = 96;
 constexpr uint MAX_ARRAY_MEMBERS = 20;
 constexpr size_t MAX_CUMULATIVE_BYTES_IN_BLOBS = 256 * MAX_ARRAY_MEMBERS;
 
