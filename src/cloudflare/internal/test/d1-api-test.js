650dbef1d64e9c1af9f85d6e1034efb99d60b414
Glen Maddern <glen@cloudflare.com>
2023-06-14 15:49:08 +1000

    Added a test for each of the D1 api functions

--- src/cloudflare/internal/test/d1-api-test.js
+++ src/cloudflare/internal/test/d1-api-test.js
@@ -1,10 +1,16 @@
 import * as assert from 'node:assert'
 
+// TODO: how to import this from d1-mock.js?
+const MOCK_USER_ROWS = {
+  1: { user_id: 1, name: 'Albert Ross', home: 'sky', features: 'wingspan' },
+  2: { user_id: 2, name: 'Al Dente', home: 'bowl', features: 'mouthfeel' },
+}
+
 export const test_d1 = {
   async test(ctr, env) {
     const DB = env.d1
     {
-      const stmt = DB.prepare(`select 1`)
+      const stmt = DB.prepare(`select 1;`)
       assert.deepEqual(await stmt.all(), {
         results: [{ 1: 1 }],
         meta: { duration: 0.001, served_by: 'd1-mock' },
@@ -14,6 +20,75 @@ export const test_d1 = {
       assert.deepEqual(await stmt.raw(), [[1]])
       assert.deepEqual(await stmt.first(), { 1: 1 })
       assert.deepEqual(await stmt.first('1'), 1)
+    }
+
+    const user_1 = MOCK_USER_ROWS[1]
+    const user_2 = MOCK_USER_ROWS[2]
+
+    {
+      const stmt = DB.prepare(`select * from users;`)
+      assert.deepEqual(await stmt.all(), {
+        results: [user_1, user_2],
+        meta: { duration: 0.001, served_by: 'd1-mock' },
+        success: true,
+      })
+
+      assert.deepEqual(await stmt.raw(), [
+        Object.values(user_1),
+        Object.values(user_2),
+      ])
+      assert.deepEqual(await stmt.first(), user_1)
+      assert.deepEqual(await stmt.first('features'), user_1.features)
+    }
+
+    const withParam = DB.prepare(`select * from users where user_id = ?;`)
+    {
+      const stmt = withParam.bind(1)
+      assert.deepEqual(await stmt.all(), {
+        results: [user_1],
+        meta: { duration: 0.001, served_by: 'd1-mock' },
+        success: true,
+      })
+
+      assert.deepEqual(await stmt.raw(), [Object.values(user_1)])
+      assert.deepEqual(await stmt.first(), user_1)
+      assert.deepEqual(await stmt.first('features'), user_1.features)
+    }
+    {
+      const stmt = withParam.bind(2)
+      assert.deepEqual(await stmt.all(), {
+        results: [user_2],
+        meta: { duration: 0.001, served_by: 'd1-mock' },
+        success: true,
+      })
+
+      assert.deepEqual(await stmt.raw(), [Object.values(user_2)])
+      assert.deepEqual(await stmt.first(), user_2)
+      assert.deepEqual(await stmt.first('features'), user_2.features)
+    }
+
+    {
+      const response = await DB.batch([withParam.bind(1), withParam.bind(2)])
+      assert.deepEqual(response, [
+        {
+          results: [user_1],
+          meta: { duration: 0.001, served_by: 'd1-mock' },
+          success: true,
+        },
+        {
+          results: [user_2],
+          meta: { duration: 0.001, served_by: 'd1-mock' },
+          success: true,
+        },
+      ])
+    }
+
+    {
+      const response = await DB.exec(`
+        select 1;
+        select * from users;
+      `)
+      assert.deepEqual(response, { count: 2, duration: 0.002 })
     }
   },
 }
--- src/cloudflare/internal/test/d1-mock.js
+++ src/cloudflare/internal/test/d1-mock.js
@@ -1,3 +1,21 @@
+const MOCK_USER_ROWS = {
+  1: { user_id: 1, name: 'Albert Ross', home: 'sky', features: 'wingspan' },
+  2: { user_id: 2, name: 'Al Dente', home: 'bowl', features: 'mouthfeel' },
+}
+
+function mockQuery({ sql, params }) {
+  switch (sql.trim()) {
+    case 'select 1;':
+      return ok({ 1: 1 })
+    case 'select * from users;':
+      return ok(...Object.values(MOCK_USER_ROWS))
+    case 'select * from users where user_id = ?;':
+      return ok(MOCK_USER_ROWS[params[0]])
+    default:
+      throw new Error(`Unmocked query: ${sql}`)
+  }
+}
+
 export default {
   async fetch(request, env, ctx) {
     try {
@@ -5,13 +23,11 @@ export default {
       const body = await request.json()
 
       if (request.method === 'POST' && pathname.startsWith('/query')) {
-        const { sql, params } = body
-        switch (sql) {
-          case 'select 1':
-            return ok({ 1: 1 })
-          default:
-            return Response.json({ error: 'Unmocked query' }, { status: 400 })
-        }
+        return Response.json(
+          Array.isArray(body)
+            ? body.map((query) => mockQuery(query))
+            : mockQuery(body)
+        )
       } else {
         return Response.json({ error: 'Not found' }, { status: 404 })
       }
@@ -25,8 +41,8 @@ export default {
 }
 
 function ok(...results) {
-  return Response.json({
+  return {
     results,
     meta: { duration: 0.001, served_by: 'd1-mock' },
-  })
+  }
 }
