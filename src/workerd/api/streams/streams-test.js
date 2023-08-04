9d28ba3163a01fe902efc11ee8bdefcdc3419e9b
Mike Aizatsky <maizatskyi@cloudflare.com>
2023-08-04 09:57:29 -0700

    [nfc] additional stream test (#974)

--- src/workerd/api/streams/streams-test.js
+++ src/workerd/api/streams/streams-test.js
@@ -2,10 +2,7 @@
 // Licensed under the Apache 2.0 license found in the LICENSE file or at:
 //     https://opensource.org/licenses/Apache-2.0
 
-import {
-  notStrictEqual,
-  strictEqual,
-} from 'node:assert';
+import * as assert from 'node:assert';
 
 export const partiallyReadStream= {
   async test(ctrl, env, ctx) {
@@ -26,3 +23,22 @@ export const partiallyReadStream= {
     await env.KV.put("key", rs);
   }
 };
+
+export const arrayBufferOfReadable = {
+  async test() {
+    const cs = new CompressionStream("gzip");
+    const cw = cs.writable.getWriter();
+    await cw.write(new TextEncoder().encode("0123456789".repeat(1000)));
+    await cw.close();
+    const data = await new Response(cs.readable).arrayBuffer();
+    assert.equal(66, data.byteLength);
+
+    const ds = new DecompressionStream("gzip");
+    const dw = ds.writable.getWriter();
+    await dw.write(data);
+    await dw.close();
+
+    const read = await new Response(ds.readable).arrayBuffer();
+    assert.equal(10_000, read.byteLength);
+  }
+}
