7a90656322bfae9ecfa8999bbfb2bfb05cd79f23
Mike Aizatsky <maizatskyi@cloudflare.com>
2023-08-16 11:22:24 -0700

    extracted to separate files

--- src/workerd/api/http.h
+++ src/workerd/api/http.h
@@ -10,6 +10,7 @@
 #include <kj/compat/http.h>
 #include <map>
 #include "basics.h"
+#include "cf-property.h"
 #include "streams.h"
 #include "form-data.h"
 #include "web-socket.h"
@@ -630,48 +631,6 @@ struct RequestInitializerDict {
     body?: BodyInit | null;
     cf?: Cf;
   });
-};
-
-class CfProperty {
-  // A holder for Cf header property value.
-  // The string header is parsed on demand and the parsed value cached.
-
-public:
-  KJ_DISALLOW_COPY(CfProperty);
-
-  explicit CfProperty() {}
-  CfProperty(decltype(nullptr)) {}
-  CfProperty(CfProperty&&) = default;
-  CfProperty& operator=(CfProperty&&) = default;
-
-  explicit CfProperty(kj::Maybe<kj::StringPtr> unparsed) {
-    KJ_IF_MAYBE(str, unparsed) {
-      value = kj::str(*str);
-    }
-  }
-
-  explicit CfProperty(kj::Maybe<jsg::V8Ref<v8::Object>>&& parsed) {
-    KJ_IF_MAYBE(v, parsed) {
-      value = kj::mv(*v);
-    }
-  }
-
-  jsg::Optional<v8::Local<v8::Object>> get(jsg::Lock& js);
-  // Get parsed value
-
-  jsg::Optional<jsg::V8Ref<v8::Object>> getRef(jsg::Lock& js);
-  // Get parsed value as a global ref
-
-  kj::Maybe<kj::String> serialize(jsg::Lock& js);
-  // Serialize to string
-
-  CfProperty deepClone(jsg::Lock& js);
-  // Clone by deep cloning parsed v8 object (if any).
-
-  void visitForGc(jsg::GcVisitor& visitor);
-
-private:
-  kj::Maybe<kj::OneOf<kj::String, jsg::V8Ref<v8::Object>>> value;
 };
 
 class Request: public Body {
