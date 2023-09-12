ef9c948acefd203bc7331755502be994f4ec06d1
Nathan Disidore <nathan@cloudflare.com>
2023-09-12 14:52:17 -0500

    VS-22: create wrapped vector search binding
    This creates a new ts defined binding for the vector search api, aka
    "Vectorize" and makes it available as an internal Cloudflare binding.

---
+++ src/cloudflare/internal/test/vectorize/vectorize-mock.js
@@ -0,0 +1,169 @@
+// Copyright (c) 2023 Cloudflare, Inc.
+// Licensed under the Apache 2.0 license found in the LICENSE file or at:
+//     https://opensource.org/licenses/Apache-2.0
+
+/** @type {Array<VectorizeMatch>} */
+const exampleVectorMatches = [
+  {
+    vectorId: "b0daca4a-ffd8-4865-926b-e24800af2a2d",
+    score: 0.71151,
+    vector: {
+      id: "b0daca4a-ffd8-4865-926b-e24800af2a2d",
+      values: [0.2331, 1.0125, 0.6131, 0.9421, 0.9661, 0.8121],
+      metadata: { text: "She sells seashells by the seashore" },
+    },
+  },
+  {
+    vectorId: "a44706aa-a366-48bc-8cc1-3feffd87d548",
+    score: 0.68913,
+    vector: {
+      id: "a44706aa-a366-48bc-8cc1-3feffd87d548",
+      values: [0.2321, 0.8121, 0.6315, 0.6151, 0.4121, 0.1512],
+      metadata: { text: "Peter Piper picked a peck of pickled peppers" },
+    },
+  },
+  {
+    vectorId: "43cfcb31-07e2-411f-8bf9-f82a95ba8b96",
+    score: 0.94812,
+    vector: {
+      id: "43cfcb31-07e2-411f-8bf9-f82a95ba8b96",
+      values: [0.0515, 0.7512, 0.8612, 0.2153, 0.15121, 0.6812],
+      metadata: {
+        text: "You know New York, you need New York, you know you need unique New York",
+      },
+    },
+  },
+];
+/** @type {Array<VectorizeVector>} */
+// @ts-ignore
+const exampleVectors = exampleVectorMatches
+  .filter((m) => typeof m !== "undefined")
+  .map((m) => m.vector);
+
+export default {
+  /**
+   * @param {Request} request
+   */
+  async fetch(request) {
+    try {
+      const { pathname } = new URL(request.url);
+
+      if (request.method === "POST" && pathname.endsWith("/create")) {
+        /** @type {VectorizeIndexConfig} */
+        const config = await request.json();
+        const name = pathname.split("/")[2];
+        /** @type {VectorizeIndexDetails} */
+        const index = {
+          id: "ffeb30f5-d349-4ba5-8dde-79da543190fe",
+          name: name || "my-index",
+          config: config,
+          vectorsCount: 0,
+        };
+        return Response.json(index);
+      } else if (request.method === "GET" && pathname.endsWith("/list")) {
+        /** @type {Array<VectorizeIndexDetails>} */
+        const index = [
+          {
+            id: "0f48d520-5bf5-4980-acd9-98453fb8a27f",
+            name: "my-first-index",
+            config: {
+              dimensions: 1536,
+              preset: "openapi-text-embedding-ada-002",
+              metric: "euclidean",
+            },
+            vectorsCount: 500000,
+          },
+          {
+            id: "b9fc84af-31f3-449c-bc61-b62abc86d5a1",
+            name: "my-second-index",
+            config: {
+              dimensions: 1536,
+              metric: "dot-product",
+            },
+            vectorsCount: 750000,
+          },
+        ];
+        return Response.json(index);
+      } else if (request.method === "GET" && pathname.split("/").length === 3) {
+        /** @type {VectorizeIndexDetails} */
+        const index = {
+          id: "ffeb30f5-d349-4ba5-8dde-79da543190fe",
+          name: pathname.split("/")[2] || "my-index",
+          config: {
+            dimensions: 1536,
+            preset: "openapi-text-embedding-ada-002",
+            metric: "euclidean",
+          },
+          vectorsCount: 850850,
+        };
+        return Response.json(index);
+      } else if (
+        request.method === "DELETE" &&
+        pathname.split("/").length === 2
+      ) {
+        return Response.json({});
+      } else if (request.method === "POST" && pathname.endsWith("/query")) {
+        /** @type {VectorizeQueryOptions & {vector: number[]}} */
+        const body = await request.json();
+        if (body?.returnVectors) {
+          return Response.json({
+            matches: exampleVectorMatches,
+            count: exampleVectorMatches.length,
+          });
+        }
+        return Response.json({
+          matches: exampleVectorMatches.map(({ vectorId, score }) => ({
+            vectorId,
+            score,
+          })),
+          count: exampleVectorMatches.length,
+        });
+      } else if (request.method === "POST" && pathname.endsWith("/insert")) {
+        /** @type {{vectors: Array<VectorizeVector>}} */
+        const data = await request.json();
+        return Response.json({
+          ids: [
+            ...data.vectors.map(({ id }) => id),
+            ...exampleVectors.map(({ id }) => id),
+          ],
+          count: data.vectors.length + exampleVectors.length,
+        });
+      } else if (request.method === "POST" && pathname.endsWith("/upsert")) {
+        /** @type {{vectors: Array<VectorizeVector>}} */
+        let data = await request.json();
+        if (data.vectors.length > 1) data.vectors.splice(-1);
+        return Response.json({
+          ids: [
+            ...data.vectors.map(({ id }) => id),
+            ...exampleVectors.map(({ id }) => id),
+          ],
+          count: data.vectors.length + exampleVectors.length,
+        });
+      } else if (
+        request.method === "POST" &&
+        pathname.endsWith("/deleteByIds")
+      ) {
+        /** @type {{ids: Array<string>}} */
+        const body = await request.json();
+        return Response.json({
+          ids: body.ids,
+          count: body.ids.length,
+        });
+      } else if (request.method === "POST" && pathname.endsWith("/getByIds")) {
+        /** @type {{ids: Array<string>}} */
+        const body = await request.json();
+        return Response.json(
+          exampleVectors.filter(({ id }) => body.ids.includes(id))
+        );
+      } else {
+        return Response.json({ error: "Not found" }, { status: 404 });
+      }
+    } catch (err) {
+      return Response.json(
+        // @ts-ignore
+        { error: err.message, stack: err.stack },
+        { status: 500 }
+      );
+    }
+  },
+};
---
+++ src/cloudflare/internal/vectorize-api.ts
@@ -0,0 +1,210 @@
+// Copyright (c) 2023 Cloudflare, Inc.
+// Licensed under the Apache 2.0 license found in the LICENSE file or at:
+//     https://opensource.org/licenses/Apache-2.0
+
+interface Fetcher {
+  fetch: typeof fetch;
+}
+
+enum Operation {
+  INDEX_GET = 0,
+  VECTOR_QUERY = 1,
+  VECTOR_INSERT = 2,
+  VECTOR_UPSERT = 3,
+  VECTOR_GET = 4,
+  VECTOR_DELETE = 5,
+}
+
+class VectorizeIndexImpl implements VectorizeIndex {
+  public constructor(
+    private readonly fetcher: Fetcher,
+    private readonly indexId: string
+  ) {}
+
+  public async describe(): Promise<VectorizeIndexDetails> {
+    const res = await this._send(
+      Operation.INDEX_GET,
+      `indexes/${this.indexId}`,
+      {
+        method: "GET",
+      }
+    );
+
+    return await toJson<VectorizeIndexDetails>(res);
+  }
+
+  public async query(
+    vector: VectorFloatArray | number[],
+    options: VectorizeQueryOptions
+  ): Promise<VectorizeMatches> {
+    const res = await this._send(
+      Operation.VECTOR_QUERY,
+      `indexes/${this.indexId}/query`,
+      {
+        method: "POST",
+        body: JSON.stringify({
+          ...options,
+          vector: Array.isArray(vector) ? vector : Array.from(vector),
+        }),
+        headers: {
+          "content-type": "application/json",
+          accept: "application/json",
+        },
+      }
+    );
+
+    return await toJson<VectorizeMatches>(res);
+  }
+
+  public async insert(
+    vectors: VectorizeVector[]
+  ): Promise<VectorizeVectorMutation> {
+    const res = await this._send(
+      Operation.VECTOR_INSERT,
+      `indexes/${this.indexId}/insert`,
+      {
+        method: "POST",
+        body: JSON.stringify({
+          vectors: vectors.map((vec) => ({
+            ...vec,
+            values: Array.isArray(vec.values)
+              ? vec.values
+              : Array.from(vec.values),
+          })),
+        }),
+        headers: {
+          "content-type": "application/json",
+          "cf-vector-search-dim-width": String(
+            vectors.length ? vectors[0]?.values?.length : 0
+          ),
+          "cf-vector-search-dim-height": String(vectors.length),
+          accept: "application/json",
+        },
+      }
+    );
+
+    return await toJson<VectorizeVectorMutation>(res);
+  }
+
+  public async upsert(
+    vectors: VectorizeVector[]
+  ): Promise<VectorizeVectorMutation> {
+    const res = await this._send(
+      Operation.VECTOR_UPSERT,
+      `indexes/${this.indexId}/upsert`,
+      {
+        method: "POST",
+        body: JSON.stringify({
+          vectors: vectors.map((vec) => ({
+            ...vec,
+            values: Array.isArray(vec.values)
+              ? vec.values
+              : Array.from(vec.values),
+          })),
+        }),
+        headers: {
+          "content-type": "application/json",
+          "cf-vector-search-dim-width": String(
+            vectors.length ? vectors[0]?.values?.length : 0
+          ),
+          "cf-vector-search-dim-height": String(vectors.length),
+          accept: "application/json",
+        },
+      }
+    );
+
+    return await toJson<VectorizeVectorMutation>(res);
+  }
+
+  public async getByIds(ids: string[]): Promise<VectorizeVector[]> {
+    const res = await this._send(
+      Operation.VECTOR_GET,
+      `indexes/${this.indexId}/getByIds`,
+      {
+        method: "POST",
+        body: JSON.stringify({ ids }),
+        headers: {
+          "content-type": "application/json",
+          accept: "application/json",
+        },
+      }
+    );
+
+    return await toJson<VectorizeVector[]>(res);
+  }
+
+  public async deleteByIds(ids: string[]): Promise<VectorizeVectorMutation> {
+    const res = await this._send(
+      Operation.VECTOR_DELETE,
+      `indexes/${this.indexId}/deleteByIds`,
+      {
+        method: "POST",
+        body: JSON.stringify({ ids }),
+        headers: {
+          "content-type": "application/json",
+          accept: "application/json",
+        },
+      }
+    );
+
+    return await toJson<VectorizeVectorMutation>(res);
+  }
+
+  private async _send(
+    operation: Operation,
+    endpoint: string,
+    init: RequestInit
+  ): Promise<Response> {
+    const res = await this.fetcher.fetch(
+      `http://vector-search/binding/${endpoint}`, // `http://vector-search` is just a dummy host, the attached fetcher will receive the request
+      init
+    );
+    if (res.status !== 200) {
+      try {
+        const err = (await res.json()) as VectorizeError;
+        throw new Error(
+          `${Operation[operation]}_ERROR ${
+            typeof err.code === "number" ? ` (code = ${err.code})` : ""
+          }: ${err.error}`,
+          {
+            cause: new Error(err.error),
+          }
+        );
+      } catch (e) {
+        throw new Error(
+          `${Operation[operation]}_ERROR: Status + ${res.status}`,
+          {
+            cause: new Error(`Status ${res.status}`),
+          }
+        );
+      }
+    }
+
+    return res;
+  }
+}
+
+const maxBodyLogChars = 1_000;
+async function toJson<T = unknown>(response: Response): Promise<T> {
+  const body = await response.text();
+  try {
+    return JSON.parse(body) as T;
+  } catch (e) {
+    throw new Error(
+      `Failed to parse body as JSON, got: ${
+        body.length > maxBodyLogChars
+          ? `${body.slice(0, maxBodyLogChars)}…`
+          : body
+      }`
+    );
+  }
+}
+
+export function makeBinding(env: {
+  fetcher: Fetcher;
+  indexId: string;
+}): VectorizeIndex {
+  return new VectorizeIndexImpl(env.fetcher, env.indexId);
+}
+
+export default makeBinding;
