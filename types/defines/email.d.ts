74a363d158fda0dd7ff106ebd8fad18cf1409146
Alex Robinson <arobinson@cloudflare.com>
2023-09-13 10:28:59 -0500

    Remove copyright headers from types/defines/*.d.ts
    As suggested by mrbbot on #1168

--- types/defines/email.d.ts
+++ types/defines/email.d.ts
@@ -1,7 +1,3 @@
-// Copyright (c) 2023 Cloudflare, Inc.
-// Licensed under the Apache 2.0 license found in the LICENSE file or at:
-//     https://opensource.org/licenses/Apache-2.0
-
 /**
  * An email message that can be sent from a Worker.
  */
--- types/defines/pubsub.d.ts
+++ types/defines/pubsub.d.ts
@@ -1,12 +1,7 @@
-// Copyright (c) 2023 Cloudflare, Inc.
-// Licensed under the Apache 2.0 license found in the LICENSE file or at:
-//     https://opensource.org/licenses/Apache-2.0
-
-// https://developers.cloudflare.com/pub-sub/
-
 // PubSubMessage represents an incoming PubSub message.
 // The message includes metadata about the broker, the client, and the payload
 // itself.
+// https://developers.cloudflare.com/pub-sub/
 interface PubSubMessage {
     // Message ID
     readonly mid: number;
