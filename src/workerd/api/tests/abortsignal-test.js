a14bb4607243719a4793ec5c963458ae28631977
James M Snell <jsnell@cloudflare.com>
2023-09-13 16:51:56 -0700

    Add wd-test unit tests for AbortSignal/AbortController (#1172)

---
+++ src/workerd/api/tests/abortsignal-test.js
@@ -0,0 +1,165 @@
+import {
+  strictEqual,
+  ok,
+  throws
+} from 'node:assert';
+
+// Test for the AbortSignal and AbortController standard Web API implementations.
+// The implementation for these are in api/basics.{h|c++}
+
+export const abortcontroller = {
+  test() {
+    // AbortSignal is not directly creatable
+    throws(() => new AbortSignal);
+
+    const ac = new AbortController();
+    ok(ac.signal instanceof AbortSignal);
+    strictEqual(ac.signal.aborted, false);
+
+    // every call to ac.signal should always be the same value.
+    strictEqual(ac.signal, ac.signal);
+
+    // signal is read only
+    throws(() => ac.signal = 1);
+
+    let invoked = 0;
+    ac.signal.onabort = (event) => {
+      invoked++;
+      strictEqual(event.type, 'abort');
+    };
+
+    // Will not throw because the signal is not aborted
+    ac.signal.throwIfAborted();
+
+    // reason and aborted are read only
+    throws(() => ac.signal.reason = 1);
+    throws(() => ac.signal.aborted = 'foo');
+
+    // trigger our abort with a default reason...
+    ac.abort();
+
+    // This one shouldn't get called since it is added after the abort
+    ac.signal.addEventListener('abort', () => {
+      throw new Error('should not have been called');
+    });
+
+    // Will throw because the signal is now aborted.
+    throws(() => ac.signal.throwIfAborted());
+
+    strictEqual(ac.signal.aborted, true);
+    strictEqual(ac.signal.reason.message, 'The operation was aborted');
+    strictEqual(ac.signal.reason.name, 'AbortError');
+
+    // Abort can be called multiple times with no effect.
+    ac.abort();
+
+    strictEqual(invoked, 1);
+  }
+};
+
+export const abortcontrollerWithReason = {
+  test() {
+    const ac = new AbortController();
+    ok(ac.signal instanceof AbortSignal);
+    strictEqual(ac.signal.aborted, false);
+
+    let invoked = 0;
+
+    ac.signal.addEventListener('abort', (event) => {
+      invoked++;
+      strictEqual(ac.signal.reason, 'foo');
+    });
+
+    ac.abort('foo');
+    strictEqual(ac.signal.aborted, true);
+    strictEqual(ac.signal.reason, 'foo');
+
+    strictEqual(invoked, 1);
+  }
+};
+
+export const alreadyAborted = {
+  test() {
+    const aborted = AbortSignal.abort();
+    strictEqual(aborted.aborted, true);
+    throws(() => aborted.throwIfAborted());
+
+    const abortedWithReason = AbortSignal.abort('foo');
+    strictEqual(abortedWithReason.aborted, true);
+    try {
+      abortedWithReason.throwIfAborted();
+      throw new Error('should have thrown');
+    } catch (err) {
+      strictEqual(err, 'foo');
+    }
+  }
+};
+
+export const timedAbort = {
+  async test() {
+    const timed = AbortSignal.timeout(100);
+    let resolve;
+    const promise = new Promise(r => resolve = r);
+    let invoked = 0;
+    timed.onabort = () => {
+      invoked++;
+      resolve();
+    };
+    await promise;
+    strictEqual(invoked, 1);
+  }
+};
+
+export const anyAbort = {
+  async test() {
+    // Set a timeout way in the future so this one doesn't happen first.
+    const timed = AbortSignal.timeout(1000000);
+    const ac = new AbortController();
+
+    // Creates an AbortSignal that will be triggered when either of the two
+    // given signals is triggered.
+    const any = AbortSignal.any([timed, ac.signal]);
+
+    let invoked = 0;
+    any.onabort = () => {
+      invoked++;
+    };
+
+    ac.abort();
+
+    strictEqual(invoked, 1);
+  }
+};
+
+export const anyAbort2 = {
+  async test() {
+    const timed = AbortSignal.timeout(100);
+    const ac = new AbortController();
+    const any = AbortSignal.any([timed, ac.signal]);
+
+    let invoked = 0;
+    let resolve;
+    const promise = new Promise(r => resolve = r);
+
+    any.onabort = () => {
+      invoked++;
+      resolve();
+    };
+
+    await promise;
+
+    strictEqual(invoked, 1);
+  }
+};
+
+export const anyAbort3 = {
+  async test() {
+    const timed = AbortSignal.timeout(1000000);
+    const aborted = AbortSignal.abort(123);
+    // If one of the signals is already abort, the any signal will be
+    // immediately aborted also.
+    const any = AbortSignal.any([timed, aborted]);
+    strictEqual(any.aborted, true);
+    strictEqual(any.reason, 123);
+  }
+};
