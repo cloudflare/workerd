8fb70a4d6ac376bc23036294f9f3cf28c4062d4d
Nathan Disidore <nathan@cloudflare.com>
2023-09-21 10:51:42 -0500

    VS-18: make Vectorize index config a generic string
    This list will grow fairly rapidly over time. As this list is only used
    in a GET-like readonly capacity, using a generic string is okay without
    sacrificing much in the way of user DX.

--- types/defines/vectorize.d.ts
+++ types/defines/vectorize.d.ts
@@ -20,15 +20,6 @@ interface VectorizeError {
 }
 
 /**
- * A pre-configured list of known models.
- * These can be supplied in place of configuring explicit dimensions.
- */
-type VectorizePreset =
-  | "openapi-text-embedding-ada-002"
-  | "workers-ai/bge-small-en"
-  | "cohere/embed-multilingual-v2.0";
-
-/**
  * Supported distance metrics for an index.
  * Distance metrics determine how other "similar" vectors are determined.
  */
@@ -49,7 +40,7 @@ type VectorizeIndexConfig =
       metric: VectorizeDistanceMetric;
     }
   | {
-      preset: VectorizePreset;
+      preset: string; // keep this generic, as we'll be adding more presets in the future and this is only in a read capacity
     };
 
 /**
