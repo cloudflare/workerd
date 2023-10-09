9131e3d18e53b061744071864f63a0cdfe8a15cd
Frederik Baetens <fbaetens@cloudflare.com>
2023-10-09 18:42:59 +0100

    JSExceptionThrown noexcept default
    Co-authored-by: Harris Hancock <harris@cloudflare.com>

--- src/workerd/jsg/util.h
+++ src/workerd/jsg/util.h
@@ -30,6 +30,7 @@ typedef unsigned int uint;
 class JsExceptionThrown: public std::exception {
 public:
   JsExceptionThrown();
+  ~JsExceptionThrown() noexcept = default;  // We must match `std::exception`'s noexcept.
   const char* what() const noexcept override;
 
 private:
