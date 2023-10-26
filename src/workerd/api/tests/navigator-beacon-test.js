3e5863a0b50878886383e3967231fb5ff359b5af
James M Snell <jsnell@cloudflare.com>
2023-10-26 08:50:45 -0700

    Implement navigator.sendBeacon (#1265)

---
+++ src/workerd/api/tests/navigator-beacon-test.js
@@ -0,0 +1,29 @@
+import {
+  ok,
+  strictEqual,
+} from 'node:assert';
+
+const enc = new TextEncoder();
+
+// This one returns false with no error thrown because we're not in an IoContext
+ok(!navigator.sendBeacon('http://example.org', 'does not work'));
+
+export const navigatorBeaconTest = {
+  async test(ctrl, env, ctx) {
+    ok(navigator.sendBeacon('http://example.org', 'beacon'));
+    ok(navigator.sendBeacon('http://example.org', new ReadableStream({
+      start(c) {
+        c.enqueue(enc.encode('beacon'));
+        c.close();
+      }
+    })));
+    ok(navigator.sendBeacon('http://example.org', enc.encode('beacon')));
+  }
+};
+
+export default {
+  async fetch(req, env) {
+    strictEqual(await req.text(), 'beacon');
+    return new Response(null, { status: 204 });
+  }
+};
