c9b1737d8181a5ef88c8d3343b9b7427e29eaaef
Hood Chatham <roberthoodchatham@gmail.com>
2023-12-04 15:18:09 -0800

    cleanup

--- src/pyodide/internal/builtin_wrappers.js
+++ src/pyodide/internal/builtin_wrappers.js
@@ -1,4 +1,7 @@
-import {default as Eval} from "pyodide-internal:eval";
+import { default as Eval } from "pyodide-internal:eval";
+
+const EXPECTED_CALLEE =
+  "    at convertJsFunctionToWasm (pyodide-internal:pyodide-bundle/pyodide.asm";
 
 function checkCallee() {
   const stack = new Error().stack;
@@ -6,10 +9,9 @@ function checkCallee() {
   // `new WebAssembly.Module()`, 4th line is callee.
   let calleeLine = stack.split("\n")[3];
   // Remove position info from line
-  const secondToLastColon = calleeLine.lastIndexOf(":", calleeLine.lastIndexOf(":") - 1);
-  calleeLine = calleeLine.slice(0, secondToLastColon);
-  if (calleeLine !== "    at convertJsFunctionToWasm (pyodide-internal:pyodide-bundle/pyodide.asm") {
-    console.warn("Unexpected call to WebAssembly.new");
+  const secondToLastColonPos = calleeLine.lastIndexOf(":", calleeLine.lastIndexOf(":") - 1);
+  calleeLine = calleeLine.slice(0, secondToLastColonPos);
+  if (calleeLine !== EXPECTED_CALLEE) {
     console.warn("$" + calleeLine + "$");
     throw new Error();
   }
@@ -18,41 +20,44 @@ function checkCallee() {
 // Wrapper for WebAssembly that makes WebAssembly.Module work by temporarily
 // turning on eval
 const origWebAssembly = globalThis.WebAssembly;
-export const WebAssembly = new Proxy(origWebAssembly, {get(target, val) {
-  const result = Reflect.get(...arguments);
-  if (val !== "Module") {
-    return result;
-  }
-  return new Proxy(result, {
-    construct() {
-      checkCallee();
-      try {
-        Eval.enableEval();
-        return Reflect.construct(...arguments);
-      } finally {
-        Eval.disableEval();
-      }
+export const WebAssembly = new Proxy(origWebAssembly, {
+  get(target, val) {
+    const result = Reflect.get(...arguments);
+    if (val !== "Module") {
+      return result;
     }
-  });
-}});
-
+    return new Proxy(result, {
+      construct() {
+        checkCallee();
+        try {
+          Eval.enableEval();
+          return Reflect.construct(...arguments);
+        } finally {
+          Eval.disableEval();
+        }
+      },
+    });
+  },
+});
 
 // Wrapper for Date.now that always advances by at least a millisecond.
 let lastTime;
 let lastDelta = 0;
 const origDate = globalThis.Date;
-export const Date = new Proxy(origDate, {get(target, val) {
-  if (val === "now") {
-    return function now() {
-      const now = origDate.now();
-      if (now === lastTime) {
-        lastDelta ++;
-      } else {
-        lastTime = now;
-        lastDelta = 0;
-      }
-      return now + lastDelta;
+export const Date = new Proxy(origDate, {
+  get(target, val) {
+    if (val === "now") {
+      return function now() {
+        const now = origDate.now();
+        if (now === lastTime) {
+          lastDelta++;
+        } else {
+          lastTime = now;
+          lastDelta = 0;
+        }
+        return now + lastDelta;
+      };
     }
-  }
-  return Reflect.get(...arguments);
-}});
+    return Reflect.get(...arguments);
+  },
+});
