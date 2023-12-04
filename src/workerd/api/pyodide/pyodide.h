51a2cbaf2b28f5999c8bca011b54d9113bf0ce04
Hood Chatham <roberthoodchatham@gmail.com>
2023-12-04 13:23:22 -0800

    Check for BUILTIN_WASM autogate

--- src/workerd/api/pyodide/pyodide.h
+++ src/workerd/api/pyodide/pyodide.h
@@ -3,15 +3,17 @@
 #include <kj/common.h>
 #include <pyodide/pyodide.capnp.h>
 #include <workerd/api/pyodide/eval.h>
+#include <workerd/util/autogate.h>
 
 namespace workerd::api::pyodide {
 
-template <class Registry>
-void registerPyodideModules(
-    Registry& registry, auto featureFlags) {
-
+template <class Registry> void registerPyodideModules(Registry& registry, auto featureFlags) {
+  if (!util::Autogate::isEnabled(util::AutogateKey::BUILTIN_WASM_MODULES)) {
+    return;
+  }
   registry.addBuiltinBundle(PYODIDE_BUNDLE, kj::none);
-  registry.template addBuiltinModule<DynEvalImpl>("pyodide-internal:eval", workerd::jsg::ModuleRegistry::Type::INTERNAL);
+  registry.template addBuiltinModule<DynEvalImpl>("pyodide-internal:eval",
+                                                  workerd::jsg::ModuleRegistry::Type::INTERNAL);
 }
 
-}
+} // namespace workerd::api::pyodide
