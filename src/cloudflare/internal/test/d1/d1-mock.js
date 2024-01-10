357766e45c494a6db41ac08586798a882252b0d9
Glen Maddern <glen@cloudflare.com>
2024-01-10 16:10:46 +1100

    Starting to plug d1-api-test into a real SQL-backed DO
    This will let us test the correctness of the D1 API much more deeply

--- src/cloudflare/internal/test/d1/d1-mock.js
+++ src/cloudflare/internal/test/d1/d1-mock.js
@@ -2,39 +2,53 @@
 // Licensed under the Apache 2.0 license found in the LICENSE file or at:
 //     https://opensource.org/licenses/Apache-2.0
 
-const MOCK_USER_ROWS = {
-  1: { user_id: 1, name: 'Albert Ross', home: 'sky', features: 'wingspan' },
-  2: { user_id: 2, name: 'Al Dente', home: 'bowl', features: 'mouthfeel' },
-}
+export class D1MockDO {
+  constructor(state, env) {
+    this.state = state
+    this.sql = this.state.storage.sql
+    this.sql.exec(`
+      CREATE TABLE IF NOT EXISTS users (
+          user_id INTEGER PRIMARY KEY,
+          name TEXT,
+          home TEXT,
+          features TEXT
+      );`)
+    this.sql.exec(`DELETE FROM users;`)
+    this.sql.exec(`INSERT INTO users (name, home, features) VALUES 
+      ('Albert Ross', 'sky', 'wingspan'),
+      ('Al Dente', 'bowl', 'mouthfeel');
+    `)
+  }
 
-function mockQuery({ sql, params }) {
-  switch (sql.trim()) {
-    case 'select 1;':
-      return ok({ 1: 1 })
-    case 'select * from users;':
-      return ok(...Object.values(MOCK_USER_ROWS))
-    case 'select * from users where user_id = ?;':
-      return ok(MOCK_USER_ROWS[params[0]])
-    default:
-      throw new Error(`Unmocked query: ${sql}`)
+  async fetch(request) {
+    const { pathname } = new URL(request.url)
+    if (request.method === 'POST' && pathname.match(/^\/(query|execute)$/)) {
+      const body = await request.json()
+      return Response.json(
+        Array.isArray(body)
+          ? body.map((query) => this.runQuery(query))
+          : this.runQuery(body)
+      )
+    } else {
+      return Response.json({ error: 'Not found' }, { status: 404 })
+    }
+  }
+
+  runQuery(query) {
+    const { sql, params = [] } = query
+    const results = Array.from(this.sql.exec(sql, ...params))
+    return {
+      results,
+      meta: { duration: 0.001, served_by: 'd1-mock' },
+    }
   }
 }
 
 export default {
   async fetch(request, env, ctx) {
     try {
-      const { pathname } = new URL(request.url)
-
-      if (request.method === 'POST' && pathname.startsWith('/query')) {
-        const body = await request.json()
-        return Response.json(
-          Array.isArray(body)
-            ? body.map((query) => mockQuery(query))
-            : mockQuery(body)
-        )
-      } else {
-        return Response.json({ error: 'Not found' }, { status: 404 })
-      }
+      const stub = env.db.get(env.db.idFromName('test'))
+      return stub.fetch(request)
     } catch (err) {
       return Response.json(
         { error: err.message, stack: err.stack },
@@ -42,11 +56,4 @@ export default {
       )
     }
   },
-}
-
-function ok(...results) {
-  return {
-    results,
-    meta: { duration: 0.001, served_by: 'd1-mock' },
-  }
 }
