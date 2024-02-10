1cb7b0b19f4c3704f2f7e8e0742a3008401a219e
Hood Chatham <roberthoodchatham@gmail.com>
2024-02-09 21:58:41 -0800

    Python: add support for subdirectory structure (#1635)

--- src/pyodide/internal/metadatafs.js
+++ src/pyodide/internal/metadatafs.js
@@ -1,32 +1,55 @@
 import { default as MetadataReader } from "pyodide-internal:runtime-generated/metadata";
 import { createReadonlyFS } from "pyodide-internal:readOnlyFS";
 
+function createTree(paths) {
+  const tree = new Map();
+  paths.forEach((elt, idx) => {
+    let subTree = tree;
+    const parts = elt.split("/");
+    const name = parts.pop();
+    for (const part of parts) {
+      let next = subTree.get(part);
+      if (!next) {
+        next = new Map();
+        subTree.set(part, next);
+      }
+      subTree = next;
+    }
+    subTree.set(name, idx);
+  });
+  return tree;
+}
+
 export function createMetadataFS(Module) {
   const TIMESTAMP = Date.now();
   const names = MetadataReader.getNames();
   const sizes = MetadataReader.getSizes();
-  const nameToIndex = new Map(names.map((val, idx) => [val, idx]));
-
+  const rootInfo = createTree(names);
   const FSOps = {
-    getNodeMode(parent, name, index) {
+    getNodeMode(parent, name, info) {
       return {
         permissions: 0o555, // read and execute but not write
-        isDir: index === undefined,
+        isDir: typeof info !== "number",
       };
     },
-    setNodeAttributes(node, index, isDir) {
+    setNodeAttributes(node, info, isDir) {
       node.modtime = TIMESTAMP;
       node.usedBytes = 0;
-      if (!isDir) {
-        node.index = index;
-        node.usedBytes = sizes[index];
+      if (info === undefined) {
+        info = rootInfo;
+      }
+      if (isDir) {
+        node.tree = info;
+      } else {
+        node.index = info;
+        node.usedBytes = sizes[info];
       }
     },
     readdir(node) {
-      return names;
+      return Array.from(node.tree.keys());
     },
     lookup(parent, name) {
-      return nameToIndex.get(name);
+      return parent.tree.get(name);
     },
     read(stream, position, buffer) {
       return MetadataReader.read(stream.node.index, position, buffer);
---
+++ src/workerd/server/tests/python/subdirectory/a.py
@@ -0,0 +1,10 @@
+from js import Response
+from subdir.a import x
+
+
+def fetch(request):
+    return Response.new("hello world")
+
+
+def test():
+    print("Hi there, this is a test", x)
