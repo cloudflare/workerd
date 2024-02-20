970dfc4714ef35edb0b068e87034e3695d7b9bed
James M Snell <jsnell@cloudflare.com>
2024-02-20 09:54:56 -0800

    Update src/workerd/api/tests/module-test.js

--- src/workerd/api/tests/module-test.js
+++ src/workerd/api/tests/module-test.js
@@ -1,7 +1,7 @@
 import * as assert from 'a/b/c';
 import * as assert3 from 'node:assert';
 
-export const abortcontroller = {
+export const basics = {
   async test() {
     const assert2 = await import('a/b/c');
     if (assert !== assert2 && assert !== assert3) {
