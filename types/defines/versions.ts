bcb19d94666b97857dbdab1d5b969889400b293d
Rahul Sethi <5822355+RamIdeas@users.noreply.github.com>
2024-03-28 11:07:45 +0000

    add WorkerVersionMetadata binding type

---
+++ types/defines/versions.ts
@@ -0,0 +1,10 @@
+/**
+ * The interface for "version_metadata" binding
+ * providing metadata about the Worker Version using this binding.
+ */
+export type WorkerVersionMetadata = {
+  /** The ID of the Worker Version using this binding */
+  id: string;
+  /** The tag of the Worker Version using this binding */
+  tag: string;
+}
