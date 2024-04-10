9a911e8994ec3e94d47a2b98491894ac8325b1c0
Garrett Gu <garrett@cloudflare.com>
2024-04-10 10:42:31 -0500

    Tweak langchain test

--- src/workerd/server/tests/python/langchain/worker.py
+++ src/workerd/server/tests/python/langchain/worker.py
@@ -5,10 +5,8 @@ part.
 
 TODO: update this to test that something happened
 """
-from langchain_core.prompts import PromptTemplate
-from langchain_openai import OpenAI
 
 def test():
-    prompt = PromptTemplate.from_template("Complete the following sentence: I am a {profession} and ")
-    llm = OpenAI(api_key="abc123")
-    chain = prompt | llm
+    from langchain_core.prompts import PromptTemplate
+    from langchain_openai import OpenAI
+
