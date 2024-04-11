ada3e35c145ab39b66864a173d785434152d8660
James M Snell <jsnell@cloudflare.com>
2024-04-11 08:43:12 -0700

    Add standard properties to TextDecoderStream (#1981)
    Add the additional standard properties to TextDecoderStream.

--- src/workerd/api/streams/encoding.h
+++ src/workerd/api/streams/encoding.h
@@ -7,6 +7,7 @@
 #include "readable.h"
 #include "writable.h"
 #include "transform.h"
+#include "../encoding.h"
 
 namespace workerd::api {
 
@@ -16,8 +17,12 @@ public:
 
   static jsg::Ref<TextEncoderStream> constructor(jsg::Lock& js);
 
+  kj::StringPtr getEncoding() { return "utf-8"_kj; }
+
   JSG_RESOURCE_TYPE(TextEncoderStream) {
     JSG_INHERIT(TransformStream);
+
+    JSG_READONLY_PROTOTYPE_PROPERTY(encoding, getEncoding);
 
     JSG_TS_OVERRIDE(extends TransformStream<string, Uint8Array>);
   }
@@ -25,24 +30,41 @@ public:
 
 class TextDecoderStream: public TransformStream {
 public:
-  using TransformStream::TransformStream;
-
   struct TextDecoderStreamInit {
     jsg::Optional<bool> fatal;
+    jsg::Optional<bool> ignoreBOM;
 
-    JSG_STRUCT(fatal);
+    JSG_STRUCT(fatal, ignoreBOM);
   };
+
+  TextDecoderStream(jsg::Ref<TextDecoder> decoder,
+                    jsg::Ref<ReadableStream> readable,
+                    jsg::Ref<WritableStream> writable);
 
   static jsg::Ref<TextDecoderStream> constructor(
       jsg::Lock& js,
       jsg::Optional<kj::String> label,
       jsg::Optional<TextDecoderStreamInit> options);
 
+  kj::StringPtr getEncoding();
+  bool getFatal();
+  bool getIgnoreBOM();
+
   JSG_RESOURCE_TYPE(TextDecoderStream) {
     JSG_INHERIT(TransformStream);
+    JSG_READONLY_PROTOTYPE_PROPERTY(encoding, getEncoding);
+    JSG_READONLY_PROTOTYPE_PROPERTY(fatal, getFatal);
+    JSG_READONLY_PROTOTYPE_PROPERTY(ignoreBOM, getIgnoreBOM);
 
     JSG_TS_OVERRIDE(extends TransformStream<ArrayBuffer | ArrayBufferView, string>);
   }
+
+  void visitForMemoryInfo(jsg::MemoryTracker& tracker) const;
+
+private:
+  jsg::Ref<TextDecoder> decoder;
+
+  void visitForGc(jsg::GcVisitor& visitor);
 };
 
 }  // namespace workerd::api
