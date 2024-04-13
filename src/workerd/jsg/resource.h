5bd8bebff072519487994ad340e312e305d3ff01
Felix Hanau <felix@cloudflare.com>
2024-04-12 20:35:54 -0400

    [nfc] Drop support for old V8 version
    This was only needed around the time of the switch from 12.0 to 12.1.

--- src/workerd/jsg/resource.h
+++ src/workerd/jsg/resource.h
@@ -21,12 +21,6 @@
 #include <workerd/jsg/memory.h>
 #include <workerd/jsg/modules.capnp.h>
 
-// The signature of SetAccessor changes in v8 12.1 to drop the v8::AccessControl
-// parameter.
-#if (V8_MAJOR_VERSION < 12) || ((V8_MAJOR_VERSION == 12) && (V8_MINOR_VERSION < 1))
-#define V8_PASS_ACCESS_CONTROL
-#endif
-
 namespace std {
   inline auto KJ_HASHCODE(const std::type_index& idx) {
     // Make std::type_index (which points to std::type_info) usable as a kj::HashMap key.
@@ -832,9 +826,6 @@ struct ResourceTypeBuilder {
         Gcb::callback,
         &SetterCallback<TypeWrapper, name, Setter, setter, isContext>::callback,
         v8::Local<v8::Value>(),
-#ifdef V8_PASS_ACCESS_CONTROL
-        v8::AccessControl::DEFAULT,
-#endif
         Gcb::enumerable ? v8::PropertyAttribute::None : v8::PropertyAttribute::DontEnum);
   }
 
@@ -876,9 +867,6 @@ struct ResourceTypeBuilder {
         &Gcb::callback,
         nullptr,
         v8::Local<v8::Value>(),
-#ifdef V8_PASS_ACCESS_CONTROL
-        v8::AccessControl::DEFAULT,
-#endif
         Gcb::enumerable ? v8::PropertyAttribute::ReadOnly
                         : static_cast<v8::PropertyAttribute>(
                             v8::PropertyAttribute::ReadOnly | v8::PropertyAttribute::DontEnum));
@@ -923,9 +911,6 @@ struct ResourceTypeBuilder {
         &Gcb::callback,
         nullptr,
         v8::Local<v8::Value>(),
-#ifdef V8_PASS_ACCESS_CONTROL
-        v8::AccessControl::DEFAULT,
-#endif
         static_cast<v8::PropertyAttribute>(
           v8::PropertyAttribute::ReadOnly | v8::PropertyAttribute::DontEnum));
   }
