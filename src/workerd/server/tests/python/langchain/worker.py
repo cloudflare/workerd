33dce06ec516c7fe844b7e6b527d39134bd183d8
Garrett Gu <garrett@cloudflare.com>
2024-05-10 11:57:08 -0500

    Use disk cache to run wd tests (#1970)
    This PR does a few things:
    1. It updates the pyodide bucket to one that includes a file called `all_wheels.zip` in its GitHub release.
    2. It uses this file to create a bazel repo that contains all the wheels
    3. It passes a path to the bazel repo as `--disk-cache-dir` when the test is being run.
    
    Open questions:
    1. Does this work on Windows
    2. Does this break tests on edgeworker somehow
    
    Why not just download the wheels from R2? The main reason is that the R2 bucket is not accessible from our build infrastructure.

--- src/workerd/server/tests/python/langchain/worker.py
+++ src/workerd/server/tests/python/langchain/worker.py
@@ -6,11 +6,7 @@ part.
 TODO: update this to test that something happened
 """
 
-
 def test():
-    from langchain.chat_models import ChatOpenAI
-    import openai
+    from langchain_core.prompts import PromptTemplate
+    from langchain_openai import OpenAI
 
-    API_KEY = "sk-abcdefgh"
-
-    ChatOpenAI(openai_api_key=API_KEY)
