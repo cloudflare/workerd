00d9cf8800f4b0905785cb1820bbbc89fb03a1ee
James M Snell <jasnell@gmail.com>
2024-09-30 07:45:11 -0700

    Start restructuring the io/trace.h to prepare for streaming trace

---
+++ src/workerd/io/trace-common.h
@@ -0,0 +1,17 @@
+#pragma once
+
+#include <workerd/io/outcome.capnp.h>
+#include <workerd/io/worker-interface.capnp.h>
+
+namespace workerd {
+
+using LogLevel = rpc::Trace::Log::Level;
+
+enum class PipelineLogLevel {
+  // WARNING: This must be kept in sync with PipelineDef::LogLevel (which is not in the OSS
+  //   release).
+  NONE,
+  FULL
+};
+
+}  // namespace workerd
