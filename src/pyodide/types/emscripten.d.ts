5de2d080323f02b70ef401bf493d691f6d73f651
Hood Chatham <roberthoodchatham@gmail.com>
2024-10-07 16:12:41 +0200

    Refactor Python startup code to prepare for Python pools
    For Python pools, we have to run `instantiateEmscriptenModule` before we bind
    the feature flags and set up the global scope and module registry. We will need
    to bundle all of the relevant code into a single script with no imports. This PR
    makes most of the typescript changes that will be needed to enable this.
    
    I split part of `python.ts` into a new file called `emscriptenSetup.ts` which
    intended for execution as part of the pool. To do this, `emscriptenSetup.ts`
    cannot import anything that uses C++ APIs. After this PR, it nearly satisfies
    this constraint except for the use of UnsafeEval by `newWasmModule`. I will fix
    that in a followup.
    
    Aside from moving code from `python.ts` to `emscriptenSetup.ts`, the code
    changes relate to the fact that `emscriptenSettings` cannot receive information
    from snapshot.ts. We have to cause the relevant effects dynamically. Instead of
    setting `INITIAL_MEMORY`, we use `Module.growMemory`. We patch Emscripten to
    read the value of `Module.noInitialRun` directly before calling `main()`
    (instead of loading it early and ignoring subsequent changes). And instead of
    calling `preloadDynamicLibs()` from a prerun hook, we use `addRunDependency()`
    to stall the initialization of the runtime until we have a chance to load the
    dynamic libraries.

--- src/pyodide/types/emscripten.d.ts
+++ src/pyodide/types/emscripten.d.ts
@@ -1,5 +1,33 @@
+interface ENV {
+  HOME: string;
+}
+
+interface API {
+  config: {
+    env: ENV;
+  };
+  finalizeBootstrap: () => void;
+  public_api: Pyodide;
+  rawRun: (code: string) => [status: number, err: string];
+}
+
+interface LDSO {
+  loadedLibsByHandle: {
+    [handle: string]: DSO;
+  };
+}
+
+interface DSO {
+  name: string;
+  refcount: number;
+  global: boolean;
+  exports: WebAssembly.Exports;
+}
+
+type PreRunHook = (mod: Module) => void;
+
 interface EmscriptenSettings {
-  preRun: ((mod: Module) => void)[];
+  preRun: PreRunHook[];
   instantiateWasm: (
     wasmImports: WebAssembly.Imports,
     successCallback: (
@@ -8,8 +36,30 @@ interface EmscriptenSettings {
     ) => void
   ) => WebAssembly.Exports;
   reportUndefinedSymbolsNoOp: () => void;
-  noInitialRun: boolean;
+  noInitialRun?: boolean;
   API: {
     config: API['config'];
   };
+  readyPromise: Promise<Module>;
+}
+
+interface Module {
+  HEAP8: Uint8Array;
+  _dump_traceback: () => void;
+  FS: FS;
+  API: API;
+  ENV: ENV;
+  LDSO: LDSO;
+  newDSO: (path: string, opt: object | undefined, handle: string) => DSO;
+  _py_version_major: () => number;
+  _py_version_minor: () => number;
+  loadWebAssemblyModule: (
+    mod: WebAssembly.Module,
+    opt: object,
+    path: string
+  ) => WebAssembly.Exports;
+  growMemory(newSize: number): void;
+  addRunDependency(x: string): void;
+  removeRunDependency(x: string): void;
+  noInitialRun: boolean;
 }
