712e77b521b644d85d13e6e9b5d8f1a4e6f9e0c8
James M Snell <jsnell@cloudflare.com>
2024-10-28 08:43:20 -0700

    Add named exports for node:path/win32 and node:path/posix (#3015)

--- src/node/path/posix.ts
+++ src/node/path/posix.ts
@@ -2,6 +2,40 @@
 // Licensed under the Apache 2.0 license found in the LICENSE file or at:
 //     https://opensource.org/licenses/Apache-2.0
 //
-
-import { posix } from 'node-internal:internal_path';
+/* eslint-disable @typescript-eslint/unbound-method */
+import { posix, win32 } from 'node-internal:internal_path';
+const {
+  basename,
+  dirname,
+  extname,
+  format,
+  isAbsolute,
+  join,
+  matchesGlob,
+  normalize,
+  parse,
+  relative,
+  resolve,
+  toNamespacedPath,
+  delimiter,
+  sep,
+} = posix;
+export {
+  basename,
+  dirname,
+  extname,
+  format,
+  isAbsolute,
+  join,
+  matchesGlob,
+  normalize,
+  parse,
+  relative,
+  resolve,
+  toNamespacedPath,
+  delimiter,
+  posix,
+  sep,
+  win32,
+};
 export default posix;
