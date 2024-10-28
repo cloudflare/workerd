b309ba9baf79b69f11ade43b50228e8d11e0975e
James M Snell <jasnell@gmail.com>
2024-10-28 07:43:26 -0700

    Add named exports for node:path/win32 and node:path/posix

--- src/node/path/win32.ts
+++ src/node/path/win32.ts
@@ -2,6 +2,40 @@
 // Licensed under the Apache 2.0 license found in the LICENSE file or at:
 //     https://opensource.org/licenses/Apache-2.0
 //
-
-import { win32 } from 'node-internal:internal_path';
+/* eslint-disable @typescript-eslint/unbound-method */
+import { win32, posix } from 'node-internal:internal_path';
+const {
+  basename,
+  dirname,
+  extname,
+  format,
+  isAbsolute,
+  join,
+  matchesGlob,
+  normalize,
+  parse,
+  relative,
+  resolve,
+  toNamespacedPath,
+  delimiter,
+  sep,
+} = win32;
+export {
+  basename,
+  dirname,
+  extname,
+  format,
+  isAbsolute,
+  join,
+  matchesGlob,
+  normalize,
+  parse,
+  relative,
+  resolve,
+  toNamespacedPath,
+  delimiter,
+  posix,
+  sep,
+  win32,
+};
 export default win32;
