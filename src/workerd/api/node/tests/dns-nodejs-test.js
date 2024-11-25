7b2290fe6cf6ce825ab268fe2d749dd616e33e22
Yagiz Nizipli <yagiz@cloudflare.com>
2024-11-25 14:43:24 -0500

    simplify tests

--- src/workerd/api/node/tests/dns-nodejs-test.js
+++ src/workerd/api/node/tests/dns-nodejs-test.js
@@ -3,24 +3,29 @@ import { strictEqual } from 'node:assert';
 
 export const functionsExist = {
   async test() {
-    strictEqual(typeof dns.lookup, 'function');
-    strictEqual(typeof dns.lookupService, 'function');
-    strictEqual(typeof dns.resolve, 'function');
-    strictEqual(typeof dns.resolve4, 'function');
-    strictEqual(typeof dns.resolve6, 'function');
-    strictEqual(typeof dns.resolveAny, 'function');
-    strictEqual(typeof dns.resolveCname, 'function');
-    strictEqual(typeof dns.resolveCaa, 'function');
-    strictEqual(typeof dns.resolveMx, 'function');
-    strictEqual(typeof dns.resolveNaptr, 'function');
-    strictEqual(typeof dns.resolveNs, 'function');
-    strictEqual(typeof dns.resolvePtr, 'function');
-    strictEqual(typeof dns.resolveSoa, 'function');
-    strictEqual(typeof dns.resolveSrv, 'function');
-    strictEqual(typeof dns.resolveTxt, 'function');
-    strictEqual(typeof dns.reverse, 'function');
-    strictEqual(typeof dns.setDefaultResultOrder, 'function');
-    strictEqual(typeof dns.getDefaultResultOrder, 'function');
-    strictEqual(typeof dns.setServers, 'function');
+    const syncFns = [
+      'lookup',
+      'lookupService',
+      'resolve',
+      'resolve4',
+      'resolve6',
+      'resolveAny',
+      'resolveCname',
+      'resolveCaa',
+      'resolveMx',
+      'resolveNaptr',
+      'resolveNs',
+      'resolvePtr',
+      'resolveSoa',
+      'resolveSrv',
+      'reverse',
+      'setDefaultResultOrder',
+      'getDefaultResultOrder',
+      'setServers',
+    ];
+
+    for (const fn of syncFns) {
+      strictEqual(typeof dns[fn], 'function');
+    }
   },
 };
