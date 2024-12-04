dae5752eed8f0dca63590a4e00e27d261f15588f
Mikkel Ricafrente <helloimalastair@users.noreply.github.com>
2024-12-03 16:06:38 -0800

    R2-2604: improve r2 binding test coverage

--- src/workerd/api/r2-test.js
+++ src/workerd/api/r2-test.js
@@ -11,15 +11,56 @@ const bufferKey = new Uint8Array([
 const hexKey =
   'b9ff919a784c7a48bf2a084056bdb94b69259b7ba59e042ade0d8734579ab5e3';
 const keyMd5 = 'WGR5pEm07DroP3hYRAh8Yw==';
+const conditionalDate = '946684800000';
 
 const objResponse = {
-  name: 'objectKey',
+  name: 'basicKey',
   version: 'objectVersion',
   size: '123',
   etag: 'objectEtag',
   uploaded: '1724767257918',
   storageClass: 'Standard',
 };
+const HeadObject = {
+  ssecKeyMd5: undefined,
+  storageClass: 'Standard',
+  range: undefined,
+  customMetadata: {},
+  httpMetadata: {},
+  uploaded: new Date(Number(objResponse.uploaded)),
+  checksums: {
+    sha512: undefined,
+    sha384: undefined,
+    sha256: undefined,
+    sha1: undefined,
+    md5: undefined,
+  },
+  httpEtag: '"objectEtag"',
+  etag: 'objectEtag',
+  size: 123,
+  version: 'objectVersion',
+  key: 'basicKey',
+};
+
+function buildGetResponse(meta, body) {
+  const encoder = new TextEncoder();
+  const metadata = encoder.encode(JSON.stringify(meta));
+  const responseBody = body
+    ? new ReadableStream({
+        start(controller) {
+          controller.enqueue(metadata);
+          controller.enqueue(encoder.encode(body));
+          controller.close();
+        },
+      })
+    : metadata;
+  return new Response(responseBody, {
+    headers: {
+      'cf-r2-metadata-size': metadata.length.toString(),
+      'content-length': metadata.length.toString(),
+    },
+  });
+}
 
 export default {
   // Handler for HTTP request binding makes to R2
@@ -50,16 +91,41 @@ export default {
 
         // Assert it's the correct version
         assert((jsonRequest.version = 1));
-        assert(
-          [
-            'put',
-            'createMultipartUpload',
-            'uploadPart',
-            'completeMultipartUpload',
-          ].includes(jsonRequest.method)
-        );
+
+        if (jsonRequest.method === 'delete') {
+          if (jsonRequest.objects) {
+            assert.deepEqual(jsonRequest.objects, ['basicKey', 'basicKey2']);
+          } else {
+            assert.deepEqual(jsonRequest.object, 'basicKey');
+          }
+          return new Response();
+        }
 
         switch (jsonRequest.object) {
+          case 'basicKey': {
+            assert.strictEqual(jsonRequest.method, 'put');
+            break;
+          }
+          case 'multiKey': {
+            if (jsonRequest.method === 'createMultipartUpload') {
+              return Response.json({
+                uploadId: 'multipartId',
+              });
+            }
+
+            if (jsonRequest.method === 'uploadPart') {
+              return Response.json({
+                etag: 'partEtag',
+              });
+            }
+            if (jsonRequest.method === 'completeMultipartUpload') {
+              return Response.json({
+                ...objResponse,
+                name: 'multiKey',
+              });
+            }
+            break;
+          }
           case 'onlyIfStrongEtag': {
             assert.deepStrictEqual(jsonRequest.onlyIf, {
               etagMatches: [
@@ -74,6 +140,7 @@ export default {
                   type: 'strong',
                 },
               ],
+              uploadedBefore: conditionalDate,
             });
             break;
           }
@@ -89,6 +156,7 @@ export default {
                   type: 'wildcard',
                 },
               ],
+              uploadedAfter: conditionalDate,
             });
             break;
           }
@@ -104,13 +172,13 @@ export default {
               },
             });
           }
-          case 'ssec-mu': {
+          case 'ssecMu': {
             if (jsonRequest.method === 'createMultipartUpload') {
               assert.deepStrictEqual(jsonRequest.ssec, {
                 key: hexKey,
               });
               return Response.json({
-                uploadId: 'definitelyARealId',
+                uploadId: 'multipartId',
               });
             }
             if (jsonRequest.method === 'uploadPart') {
@@ -118,7 +186,7 @@ export default {
                 key: hexKey,
               });
               return Response.json({
-                etag: 'definitelyAValidEtag',
+                etag: 'partEtag',
                 ssec: {
                   algorithm: 'aes256',
                   keyMd5,
@@ -142,102 +210,219 @@ export default {
         const rawHeader = request.headers.get('cf-r2-request');
         const jsonRequest = JSON.parse(rawHeader);
         assert((jsonRequest.version = 1));
-        assert(['get', 'head'].includes(jsonRequest.method));
-        if (jsonRequest.object === 'ssec') {
-          const encoder = new TextEncoder();
-          const metadata = encoder.encode(
-            JSON.stringify({
-              ...objResponse,
-              ssec: {
-                algorithm: 'aes256',
-                keyMd5,
-              },
-            })
-          );
-          const body =
-            jsonRequest.method === 'get'
-              ? new ReadableStream({
-                  start(controller) {
-                    controller.enqueue(metadata);
-                    controller.enqueue(encoder.encode('Bonk'));
-                    controller.close();
-                  },
-                })
-              : metadata;
-          return new Response(body, {
-            headers: {
-              'cf-r2-metadata-size': metadata.length.toString(),
-              'content-length': metadata.length.toString(),
-            },
+        if (jsonRequest.method === 'list') {
+          assert.deepEqual(jsonRequest, {
+            version: 1,
+            method: 'list',
+            limit: 1,
+            prefix: 'basic',
+            cursor: 'ai',
+            delimiter: '/',
+            include: [0, 1],
+            newRuntime: true,
           });
+          return buildGetResponse({
+            objects: [objResponse],
+            truncated: false,
+            cursor: 'ai',
+            deliminatedPrefixes: [],
+          });
+        }
+        assert(['get', 'head'].includes(jsonRequest.method));
+        switch (jsonRequest.object) {
+          case 'basicKey': {
+            return buildGetResponse(objResponse, 'content');
+          }
+          case 'onlyIfStrongEtag': {
+            assert.deepStrictEqual(jsonRequest.onlyIf, {
+              etagMatches: [
+                {
+                  value: 'strongEtag',
+                  type: 'strong',
+                },
+              ],
+              etagDoesNotMatch: [
+                {
+                  value: 'strongEtag',
+                  type: 'strong',
+                },
+              ],
+              uploadedBefore: conditionalDate,
+            });
+            return buildGetResponse(objResponse, 'content');
+          }
+          case 'onlyIfWildcard': {
+            assert.deepStrictEqual(jsonRequest.onlyIf, {
+              etagMatches: [
+                {
+                  type: 'wildcard',
+                },
+              ],
+              etagDoesNotMatch: [
+                {
+                  type: 'wildcard',
+                },
+              ],
+              uploadedAfter: conditionalDate,
+            });
+            return buildGetResponse(objResponse, 'content');
+          }
+          case 'ssec': {
+            return buildGetResponse(
+              {
+                ...objResponse,
+                ssec: {
+                  algorithm: 'aes256',
+                  keyMd5,
+                },
+              },
+              'content'
+            );
+          }
         }
       }
     }
     throw new Error('unexpected');
   },
-
   async test(ctrl, env, ctx) {
+    // Basic Operations
     {
-      // Conditionals
-      await env.BUCKET.put('basic', 'content');
+      // Destructuring syntax looks ugly, but gets around needing to construct HeadResponse objects(somehow?)
+      // PutObject
+      {
+        const { ...obj } = await env.BUCKET.put('basicKey', 'content');
+        obj.checksums = { ...obj.checksums };
+        assert.deepEqual(obj, HeadObject);
+      }
+      // GetObject
+      {
+        const objWithBody = await env.BUCKET.get('basicKey');
+        const { ...obj } = objWithBody;
+        obj.checksums = { ...obj.checksums };
+        assert.deepEqual(obj, HeadObject);
+        assert.deepEqual(await objWithBody.text(), 'content');
+      }
+      // HeadObject
+      {
+        const { ...obj } = await env.BUCKET.head('basicKey');
+        obj.checksums = { ...obj.checksums };
+        assert.deepEqual(obj, HeadObject);
+      }
+      // MultipartUploads
+      {
+        // CreateMultipartUpload
+        const multi = await env.BUCKET.createMultipartUpload('multiKey');
+        assert.equal(multi.uploadId, 'multipartId');
+        assert.equal(multi.key, 'multiKey');
+        // UploadPart
+        const part = await multi.uploadPart(1, 'content');
+        assert.equal(part.etag, 'partEtag');
+        // CompleteMultipartUpload
+        const { ...obj } = await multi.complete([
+          {
+            partNumber: 1,
+            etag: 'partEtag',
+          },
+        ]);
+        obj.checksums = { ...obj.checksums };
+        assert.deepEqual(obj, { ...HeadObject, key: 'multiKey' });
+      }
+      // ListObjects
+      {
+        const list = await env.BUCKET.list({
+          limit: 1,
+          prefix: 'basic',
+          cursor: 'ai',
+          delimiter: '/',
+          include: ['httpMetadata', 'customMetadata'],
+        });
+        list.objects[0] = { ...list.objects[0] };
+        list.objects[0].checksums = { ...list.objects[0].checksums };
+        assert.deepEqual(list, {
+          objects: [HeadObject],
+          truncated: false,
+          cursor: 'ai',
+          delimitedPrefixes: [],
+        });
+      }
+      // DeleteObject
+      {
+        await env.BUCKET.delete('basicKey');
+        await env.BUCKET.delete(['basicKey', 'basicKey2']);
+      }
+    }
+    // Conditionals
+    {
       await env.BUCKET.put('onlyIfStrongEtag', 'content', {
         onlyIf: {
           etagMatches: 'strongEtag',
           etagDoesNotMatch: 'strongEtag',
+          uploadedBefore: new Date('0'),
         },
       });
       await env.BUCKET.put('onlyIfWildcard', 'content', {
         onlyIf: {
           etagMatches: '*',
           etagDoesNotMatch: '*',
+          uploadedAfter: new Date('0'),
+        },
+      });
+      await env.BUCKET.get('onlyIfStrongEtag', {
+        onlyIf: {
+          etagMatches: 'strongEtag',
+          etagDoesNotMatch: 'strongEtag',
+          uploadedBefore: new Date('0'),
+        },
+      });
+      await env.BUCKET.get('onlyIfWildcard', {
+        onlyIf: {
+          etagMatches: '*',
+          etagDoesNotMatch: '*',
+          uploadedAfter: new Date('0'),
         },
       });
     }
-
+    // SSEC
     {
-      // SSEC
       for (const ssecKey of [bufferKey, hexKey]) {
+        // PutObject
         {
           const { ssecKeyMd5 } = await env.BUCKET.put('ssec', 'content', {
             ssecKey,
           });
           assert.strictEqual(ssecKeyMd5, keyMd5);
         }
+        // GetObject
         {
           const { ssecKeyMd5 } = await env.BUCKET.get('ssec', {
             ssecKey,
           });
           assert.strictEqual(ssecKeyMd5, keyMd5);
         }
+        // HeadObject
         {
           const { ssecKeyMd5 } = await env.BUCKET.head('ssec', {
             ssecKey,
           });
           assert.strictEqual(ssecKeyMd5, keyMd5);
         }
+        // MultipartUpload
         {
-          const multi = await env.BUCKET.createMultipartUpload('ssec-mu', {
+          // CreateMultipartUpload
+          const multi = await env.BUCKET.createMultipartUpload('ssecMu', {
             ssecKey,
           });
-          assert.equal(multi.uploadId, 'definitelyARealId');
-        }
-        {
-          const multi = await env.BUCKET.createMultipartUpload('ssec-mu', {
-            ssecKey,
-          });
+          assert.equal(multi.uploadId, 'multipartId');
+          // UploadPart
           const part = await multi.uploadPart(1, 'hey', {
             ssecKey,
           });
-          assert.equal(part.etag, 'definitelyAValidEtag');
-        }
-        {
-          const multi = await env.BUCKET.createMultipartUpload('ssec-mu', {
-            ssecKey,
-          });
+          assert.equal(part.etag, 'partEtag');
+          // CompleteMultipartUpload
           const { ssecKeyMd5 } = await multi.complete([
             {
               partNumber: 1,
-              etag: 'definitelyAValidEtag',
+              etag: 'partEtag',
             },
           ]);
           assert.strictEqual(ssecKeyMd5, keyMd5);
