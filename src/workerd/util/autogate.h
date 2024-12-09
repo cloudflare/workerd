d6a970204ca75d361528c1424c3c4399ef0351aa
James M Snell <jsnell@cloudflare.com>
2024-12-09 09:20:53 -0800

    Add the streaming-tail-workers autogate

--- src/workerd/util/autogate.h
+++ src/workerd/util/autogate.h
@@ -14,6 +14,7 @@ namespace workerd::util {
 // Workerd-specific list of autogate keys (can also be used in internal repo).
 enum class AutogateKey {
   TEST_WORKERD,
+  STREAMING_TAIL_WORKERS,
   NumOfKeys  // Reserved for iteration.
 };
 
