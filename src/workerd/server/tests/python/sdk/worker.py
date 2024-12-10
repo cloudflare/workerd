0481b0fc2c534a8b3f07a67250af6e0ebcc7d95a
Dominik Picheta <dominik@cloudflare.com>
2024-12-10 17:55:14 +0000

    Implements Response.replace_body and support for cf opts in Py fetch.

--- src/workerd/server/tests/python/sdk/worker.py
+++ src/workerd/server/tests/python/sdk/worker.py
@@ -1,9 +1,25 @@
+from contextlib import asynccontextmanager
 from http import HTTPMethod, HTTPStatus
 
 import js
 
+import pyodide.http
 from cloudflare.workers import Blob, File, FormData, Response, fetch
 from pyodide.ffi import to_js
+
+
+@asynccontextmanager
+async def _mock_fetch(check):
+    async def mocked_fetch(original_fetch, url, opts):
+        check(url, opts)
+        return await original_fetch(url, opts)
+
+    original_fetch = pyodide.http._jsfetch
+    pyodide.http._jsfetch = lambda url, opts: mocked_fetch(original_fetch, url, opts)
+    try:
+        yield
+    finally:
+        pyodide.http._jsfetch = original_fetch
 
 
 # Each path in this handler is its own test. The URLs that are being fetched
@@ -51,8 +67,18 @@ async def on_fetch(request):
     elif request.url.endswith("/undefined_opts"):
         # This tests two things:
         #   * `Response.redirect` static method
-        #   * that other options can be passed into `fetch`
-        resp = await fetch("https://example.com/redirect", redirect="manual")
+        #   * that other options can be passed into `fetch` (so that we can support
+        #       new options without updating this code)
+
+        # Mock pyodide.http._jsfetch to ensure `foobarbaz` gets passed in.
+        def fetch_check(url, opts):
+            assert opts.foobarbaz == 42
+
+        async with _mock_fetch(fetch_check):
+            resp = await fetch(
+                "https://example.com/redirect", redirect="manual", foobarbaz=42
+            )
+
         return resp
     elif request.url.endswith("/response_inherited"):
         expected = "test123"
@@ -89,6 +115,14 @@ async def on_fetch(request):
         assert data["blob.py"].content_type == "text/python"
         assert data["metadata"].name == "metadata.json"
 
+        return Response("success")
+    elif request.url.endswith("/cf_opts"):
+        resp = await fetch(
+            "http://example.com/redirect",
+            redirect="manual",
+            cf={"cacheTtl": 5, "cacheEverything": True, "cacheKey": "someCustomKey"},
+        )
+        assert resp.status == 301
         return Response("success")
     else:
         resp = await fetch("https://example.com/sub")
@@ -273,6 +307,23 @@ async def can_request_form_data_blob(env):
     assert text == "success"
 
 
+async def replace_body_unit_tests(env):
+    response = Response("test", status=201, status_text="Created")
+    cloned = response.replace_body("other")
+    assert cloned.status == 201
+    assert cloned.status_text == "Created"
+    t = await cloned.text()
+    assert t == "other"
+
+
+async def can_use_cf_fetch_opts(env):
+    response = await env.SELF.fetch(
+        "http://example.com/cf_opts",
+    )
+    text = await response.text()
+    assert text == "success"
+
+
 async def test(ctrl, env):
     await can_return_custom_fetch_response(env)
     await can_modify_response(env)
@@ -287,3 +338,5 @@ async def test(ctrl, env):
     await form_data_unit_tests(env)
     await blob_unit_tests(env)
     await can_request_form_data_blob(env)
+    await replace_body_unit_tests(env)
+    await can_use_cf_fetch_opts(env)
