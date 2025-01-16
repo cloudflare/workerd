43139f0dd6ed880c314b45972be79dfeb3c3d223
Dominik Picheta <dominik@cloudflare.com>
2025-01-16 17:24:34 +0000

    Fixes Python SDK tests when external package loading is enabled.

---
+++ src/workerd/server/tests/python/sdk/proxy.js
@@ -0,0 +1,12 @@
+export default {
+  async fetch(req, env) {
+    const url = new URL(req.url);
+    if (url.hostname == 'pyodide-packages.runtime-playground.workers.dev') {
+      return env.INTERNET.fetch(req);
+    } else if (url.hostname == 'example.com') {
+      return env.PYTHON.fetch(req);
+    }
+
+    throw new Error('Invalid url: ' + url);
+  },
+};
