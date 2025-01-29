0470f10b7f941d73a77f3511633b1764b8e5f474
Nicholas Paun <npaun@cloudflare.com>
2025-01-29 06:58:09 -0800

    WPT: Add dom/abortsignal tests
    Try to fix Windows builds; drops files belonging to another target from include

--- src/workerd/api/wpt/eslint.config.mjs
+++ src/workerd/api/wpt/eslint.config.mjs
@@ -3,7 +3,7 @@ import { baseConfig } from '../../../../tools/base.eslint.config.mjs';
 export default [
   ...baseConfig({ tsconfigRootDir: import.meta.dirname }),
   {
-    files: ['src/workerd/api/wpt/*-test.js'],
+    files: ['src/workerd/api/wpt/**/*-test.ts'],
     rules: {
       'sort-keys': 'error',
     },
