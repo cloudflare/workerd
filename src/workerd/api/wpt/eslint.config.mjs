bbe8a9651ca83d9c33c627d5f743b24e199e88af
Nicholas Paun <np@icebergsystems.ca>
2025-02-04 09:37:11 -0800

    Revert "WPT: Add dom/abortsignal tests (#3436)"
    This reverts commit 75d6a7694df482da7ed365fc5357a7f646fe68c7.

--- src/workerd/api/wpt/eslint.config.mjs
+++ src/workerd/api/wpt/eslint.config.mjs
@@ -3,7 +3,7 @@ import { baseConfig } from '../../../../tools/base.eslint.config.mjs';
 export default [
   ...baseConfig({ tsconfigRootDir: import.meta.dirname }),
   {
-    files: ['src/workerd/api/wpt/**/*-test.ts'],
+    files: ['src/workerd/api/wpt/*-test.js'],
     rules: {
       'sort-keys': 'error',
     },
