fc16ad237745efd604a71ebbb003b525af9587c1
Harry Hough <hhough@cloudflare.com>
2025-02-05 14:19:41 -0600

    Change PipelineTransform to PipelineTransformationEntrypoint class implementation

--- types/defines/pipeline-transform.d.ts
+++ types/defines/pipeline-transform.d.ts
@@ -2,12 +2,23 @@
 // Licensed under the Apache 2.0 license found in the LICENSE file or at:
 //     https://opensource.org/licenses/Apache-2.0
 
-export abstract class PipelineTransform {
+export type PipelineRecord<T> = {
+  body: T;
+  readonly metadata: object;
+};
+
+export type PipelineBatchMetadata = {
+  pipelineId: string;
+  pipelineName: string;
+};
+
+export abstract class PipelineTransformationEntrypoint<I, O> {
   /**
-   * transformJson recieves an array of javascript objects which can be
+   * run recieves an array of PipelineRecord which can be
    * mutated and returned to the pipeline
-   * @param data The data to be mutated
-   * @returns A promise containing the mutated data
+   * @param records Incoming records from the pipeline to be transformed
+   * @param metadata Information about the specific pipeline calling the transformation entrypoint
+   * @returns A promise containing the transformed PipelineRecord array
    */
-  public transformJson(data: object[]): Promise<object[]>;
+  public run(records: PipelineRecord<I>[], metadata: PipelineBatchMetadata): Promise<PipelineRecord<O>[]>;
 }
