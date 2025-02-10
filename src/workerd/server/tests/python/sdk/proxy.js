7ef3534089efd6a71a4ce57ff5f7f927924823fc
Hood Chatham <roberthoodchatham@gmail.com>
2025-02-10 18:15:48 +0100

    Fix url in python sdk proxy.js

--- src/workerd/server/tests/python/sdk/proxy.js
+++ src/workerd/server/tests/python/sdk/proxy.js
@@ -1,7 +1,7 @@
 export default {
   async fetch(req, env) {
     const url = new URL(req.url);
-    if (url.hostname == 'pyodide-packages.runtime-playground.workers.dev') {
+    if (url.hostname == 'python-packages.edgeworker.net') {
       return env.INTERNET.fetch(req);
     } else if (url.hostname == 'example.com') {
       return env.PYTHON.fetch(req);
