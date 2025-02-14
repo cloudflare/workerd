8885a023874d42a1c2a13728fd71b98918b91c37
Nicholas Paun <npaun@cloudflare.com>
2025-02-14 15:54:14 -0800

    Move all wpt code into //src/wpt

--- src/wpt/eslint.config.mjs
+++ src/wpt/eslint.config.mjs
@@ -1,3 +1,11 @@
 import { baseConfig } from '../../tools/base.eslint.config.mjs';
 
-export default [...baseConfig({ tsconfigRootDir: import.meta.dirname })];
+export default [
+  ...baseConfig({ tsconfigRootDir: import.meta.dirname }),
+  {
+    files: ['src/wpt/**/*-test.ts'],
+    rules: {
+      'sort-keys': 'error',
+    },
+  },
+];
--- src/wpt/urlpattern-test.ts
+++ src/wpt/urlpattern-test.ts
@@ -2,7 +2,7 @@
 // Licensed under the Apache 2.0 license found in the LICENSE file or at:
 //     https://opensource.org/licenses/Apache-2.0
 
-import { type TestRunnerConfig } from 'wpt:harness';
+import { type TestRunnerConfig } from 'harness/harness';
 
 export default {
   'urlpattern-compare.tentative.any.js': {
