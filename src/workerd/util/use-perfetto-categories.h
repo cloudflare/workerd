7d89464a0cb3a8b5d04776397b751607a426df0e
Harris Hancock <harris@cloudflare.com>
2025-02-24 11:42:48 +0000

    Fix typo in Perfetto comment

--- src/workerd/util/use-perfetto-categories.h
+++ src/workerd/util/use-perfetto-categories.h
@@ -5,7 +5,7 @@
 // This header is to be imported by any translation unit (c++ file) that
 // is instrumenting with perfetto traces (e.g. TRACE_EVENT). Header files
 // that include instrumentation but are imported by embedders should instead
-// use PERFETTO_USE_CATEGORY_FROM_NAMESPACE_SCOPED within function or block
+// use PERFETTO_USE_CATEGORIES_FROM_NAMESPACE_SCOPED within function or block
 // scopes that are to be instrumented.
 
 #if defined(WORKERD_USE_PERFETTO)
