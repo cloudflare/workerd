57ae2483ecac4f42dc7689b30508f61fe4cb4c53
Yagiz Nizipli <yagiz@cloudflare.com>
2025-03-06 15:34:47 -0500

    implement TLSSocket and connect from node:tls

--- src/node/tls.ts
+++ src/node/tls.ts
@@ -23,59 +23,9 @@
 // OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE
 // USE OR OTHER DEALINGS IN THE SOFTWARE.
 
-import {
-  BlockList,
-  SocketAddress,
-  Server,
-  Socket,
-  connect,
-  createConnection,
-  createServer,
-  getDefaultAutoSelectFamily,
-  setDefaultAutoSelectFamily,
-  getDefaultAutoSelectFamilyAttemptTimeout,
-  setDefaultAutoSelectFamilyAttemptTimeout,
-  isIP,
-  isIPv4,
-  isIPv6,
-  _normalizeArgs,
-} from 'node-internal:internal_net';
-
-export const Stream = Socket;
-
-export {
-  BlockList,
-  SocketAddress,
-  Server,
-  Socket,
-  connect,
-  createConnection,
-  createServer,
-  getDefaultAutoSelectFamily,
-  setDefaultAutoSelectFamily,
-  getDefaultAutoSelectFamilyAttemptTimeout,
-  setDefaultAutoSelectFamilyAttemptTimeout,
-  isIP,
-  isIPv4,
-  isIPv6,
-  _normalizeArgs,
-};
-
+import { TLSSocket, connect } from 'node-internal:internal_tls_wrap';
+export { TLSSocket, connect };
 export default {
-  BlockList,
-  SocketAddress,
-  Stream: Socket,
-  Server,
-  Socket,
+  TLSSocket,
   connect,
-  createConnection,
-  createServer,
-  getDefaultAutoSelectFamily,
-  setDefaultAutoSelectFamily,
-  getDefaultAutoSelectFamilyAttemptTimeout,
-  setDefaultAutoSelectFamilyAttemptTimeout,
-  isIP,
-  isIPv4,
-  isIPv6,
-  _normalizeArgs,
 };
