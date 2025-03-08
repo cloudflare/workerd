54889c76e630e7603babbe9a5fee20cae1343cc0
Kenton Varda <kenton@cloudflare.com>
2025-03-08 00:06:51 -0600

    Regenerate types for `encodeResponseBody`.

--- types/generated-snapshot/2022-03-21/index.ts
+++ types/generated-snapshot/2022-03-21/index.ts
@@ -1715,6 +1715,7 @@ export interface RequestInit<Cf = CfProperties> {
   integrity?: string;
   /* An AbortSignal to set request's signal. */
   signal?: AbortSignal | null;
+  encodeResponseBody?: "automatic" | "manual";
 }
 export type Service<
   T extends Rpc.WorkerEntrypointBranded | undefined = undefined,
