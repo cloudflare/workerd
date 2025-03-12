3ef14ba20c5424d8f12060e3ba76ca26760ce579
Lambros Petrou <lambros@cloudflare.com>
2025-03-12 10:47:49 +0000

    update types/generated-snapshots
    Run: bazel build //types:types && rm -rf types/generated-snapshot && cp -r bazel-bin/types/definitions types/generated-snapshot

--- types/generated-snapshot/2023-07-01/index.d.ts
+++ types/generated-snapshot/2023-07-01/index.d.ts
@@ -5463,11 +5463,47 @@ interface D1ExecResult {
   count: number;
   duration: number;
 }
+type D1SessionConstraint =
+  // Indicates that the first query should go to the primary, and the rest queries
+  // using the same D1DatabaseSession will go to any replica that is consistent with
+  // the bookmark maintained by the session (returned by the first query).
+  | "first-primary"
+  // Indicates that the first query can go anywhere (primary or replica), and the rest queries
+  // using the same D1DatabaseSession will go to any replica that is consistent with
+  // the bookmark maintained by the session (returned by the first query).
+  | "first-unconstrained";
+type D1SessionBookmark = string;
 declare abstract class D1Database {
   prepare(query: string): D1PreparedStatement;
-  dump(): Promise<ArrayBuffer>;
   batch<T = unknown>(statements: D1PreparedStatement[]): Promise<D1Result<T>[]>;
   exec(query: string): Promise<D1ExecResult>;
+  /**
+   * Creates a new D1 Session anchored at the given constraint or the bookmark.
+   * All queries executed using the created session will have sequential consistency,
+   * meaning that all writes done through the session will be visible in subsequent reads.
+   *
+   * @param constraintOrBookmark Either the session constraint or the explicit bookmark to anchor the created session.
+   */
+  withSession(
+    constraintOrBookmark:
+      | D1SessionBookmark
+      | D1SessionConstraint
+      | null
+      | undefined,
+  ): D1DatabaseSession;
+  /**
+   * @deprecated dump() will be removed soon, only applies to deprecated alpha v1 databases.
+   */
+  dump(): Promise<ArrayBuffer>;
+}
+declare abstract class D1DatabaseSession {
+  prepare(query: string): D1PreparedStatement;
+  batch<T = unknown>(statements: D1PreparedStatement[]): Promise<D1Result<T>[]>;
+  /**
+   * @returns The latest session bookmark across all executed queries on the session.
+   *          If no query has been executed yet, `null` is returned.
+   */
+  getBookmark(): D1SessionBookmark | null;
 }
 declare abstract class D1PreparedStatement {
   bind(...values: unknown[]): D1PreparedStatement;
