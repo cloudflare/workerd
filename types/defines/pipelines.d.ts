782d686cf3b53d08b463dcbe06d87d6174abe3e6
Harry Hough <hhough@cloudflare.com>
2025-03-19 15:00:57 -0500

    Updated cloudflare:pipelines binding & transformation defaults for generic params

--- types/defines/pipelines.d.ts
+++ types/defines/pipelines.d.ts
@@ -2,19 +2,18 @@
 // Licensed under the Apache 2.0 license found in the LICENSE file or at:
 //     https://opensource.org/licenses/Apache-2.0
 declare module "cloudflare:pipelines" {
-  export abstract class PipelineTransformationEntrypoint<Env = unknown, I extends PipelineRecord = {}, O extends PipelineRecord = {}> {
-    /**
-     * run recieves an array of PipelineRecord which can be
-     * mutated and returned to the pipeline
-     * @param records Incoming records from the pipeline to be transformed
-     * @param metadata Information about the specific pipeline calling the transformation entrypoint
-     * @returns A promise containing the transformed PipelineRecord array
-     */
-
+  export abstract class PipelineTransformationEntrypoint<Env = unknown, I extends PipelineRecord = PipelineRecord, O extends PipelineRecord = PipelineRecord> {
     protected env: Env;
     protected ctx: ExecutionContext;
     constructor(ctx: ExecutionContext, env: Env);
 
+    /**
+     * run recieves an array of PipelineRecord which can be
+     * transformed and returned to the pipeline
+     * @param records Incoming records from the pipeline to be transformed
+     * @param metadata Information about the specific pipeline calling the transformation entrypoint
+     * @returns A promise containing the transformed PipelineRecord array
+     */
     public run(records: I[], metadata: PipelineBatchMetadata): Promise<O[]>;
   }
   export type PipelineRecord = Record<string, unknown>
@@ -22,7 +21,7 @@ declare module "cloudflare:pipelines" {
     pipelineId: string;
     pipelineName: string;
   }
-  export interface Pipeline<T extends PipelineRecord> {
+  export interface Pipeline<T extends PipelineRecord = PipelineRecord> {
     /**
      * The Pipeline interface represents the type of a binding to a Pipeline
      *
