0777464f414ae59f7317be9ccdac5ee9c5917a35
James M Snell <jsnell@cloudflare.com>
2025-04-09 16:27:29 -0700

    Update samples/helloworld_esm/worker.js

--- samples/helloworld_esm/worker.js
+++ samples/helloworld_esm/worker.js
@@ -4,23 +4,6 @@
 
 export default {
   async fetch(req, env) {
-
-    const headers = new Headers();
-    headers.set('content-type', 'application/x-www-form-urlencoded');
-    const req2 = new Request('http://example.org', {
-      method: 'POST',
-      headers,
-      body: [
-        'field0=part0',
-        'field1=part1',
-        'field0=part2',
-        'field1=part3',
-      ].join('&')
-    });
-
-    const fd = await req2.formData();
-    for (const [k,v] of fd) console.log(k,v);
-
     return new Response("Hello World\n");
   }
 };
