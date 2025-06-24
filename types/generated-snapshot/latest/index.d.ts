d2046cb583f25952a259250dcfd159aafaf82da8
Somhairle MacLeòid <smacleod@cloudflare.com>
2025-06-24 16:12:07 +0100

    Create type for a Hello World Binding (#4245)
    * Create hello-world.d.ts
    
    * fix snapshot

--- types/generated-snapshot/latest/index.d.ts
+++ types/generated-snapshot/latest/index.d.ts
@@ -7288,6 +7288,22 @@ declare module "cloudflare:email" {
   };
   export { _EmailMessage as EmailMessage };
 }
+/**
+ * Hello World binding to serve as an explanatory example. DO NOT USE
+ */
+interface HelloWorldBinding {
+  /**
+   * Retrieve the current stored value
+   */
+  get(): Promise<{
+    value: string;
+    ms?: number;
+  }>;
+  /**
+   * Set a new stored value
+   */
+  set(value: string): Promise<void>;
+}
 interface Hyperdrive {
   /**
    * Connect directly to Hyperdrive as if it's your database, returning a TCP socket.
