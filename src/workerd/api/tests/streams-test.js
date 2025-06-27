a2142f287671aad29d55346558339f636b3d907d
Phil Pluckthun <phil@kitten.sh>
2025-06-27 15:22:30 +0100

    Add failing {rs,ts}Request tests to `workerd/api/tests/streams-test.js`

--- src/workerd/api/tests/streams-test.js
+++ src/workerd/api/tests/streams-test.js
@@ -36,6 +36,44 @@ export const ts = {
   },
 };
 
+export const rsRequest = {
+  async test(ctrl, env) {
+    const resp = await env.subrequest.fetch(
+      new Request('http://example.org', {
+        method: 'POST',
+        body: new ReadableStream({
+          expectedLength: 10,
+          start(c) {
+            c.enqueue(enc.encode('hellohello'));
+            c.close();
+          },
+        }),
+      })
+    );
+    for await (const _ of resp.body) {
+    }
+  },
+};
+
+export const tsRequest = {
+  async test(ctrl, env) {
+    const { readable, writable } = new TransformStream({
+      expectedLength: 10,
+    });
+    const writer = writable.getWriter();
+    writer.write(enc.encode('hellohello'));
+    writer.close();
+    const resp = await env.subrequest.fetch(
+      new Request('http://example.org', {
+        method: 'POST',
+        body: readable,
+      })
+    );
+    for await (const _ of resp.body) {
+    }
+  },
+};
+
 export const byobMin = {
   async test() {
     let controller;
