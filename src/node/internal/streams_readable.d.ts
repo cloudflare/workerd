8f97902a5fed25f4d33379f0b68c6a49bee0dbf3
Yagiz Nizipli <yagiz@nizipli.com>
2025-06-30 16:31:06 -0400

    improve some typescript errors

--- src/node/internal/streams_readable.d.ts
+++ src/node/internal/streams_readable.d.ts
@@ -1,7 +1,10 @@
+// Copyright (c) 2017-2022 Cloudflare, Inc.
+// Licensed under the Apache 2.0 license found in the LICENSE file or at:
+//     https://opensource.org/licenses/Apache-2.0
+
 import { Readable as _Readable } from 'node:stream';
 
 export declare class Readable extends _Readable {
-  destroyed: boolean;
   _readableState?: {
     readingMore: boolean;
   };
