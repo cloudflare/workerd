9f32a4d78834580558190539bd6f04aad81fd308
Guy Bedford <gbedford@cloudflare.com>
2025-07-02 12:58:02 -0700

    remove events filtering

--- src/node/events.ts
+++ src/node/events.ts
@@ -3,20 +3,5 @@
 //     https://opensource.org/licenses/Apache-2.0
 //
 
-// Explicit list used to hide _initialized private export
-export {
-  EventEmitter,
-  EventEmitterAsyncResource,
-  addAbortListener,
-  captureRejectionSymbol,
-  errorMonitor,
-  getMaxListeners,
-  usingDomains,
-  defaultMaxListeners,
-  setMaxListeners,
-  listenerCount,
-  getEventListeners,
-  once,
-  on,
-} from 'node-internal:events';
+export * from 'node-internal:events';
 export { default } from 'node-internal:events';
