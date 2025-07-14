178e1c22c1a37921fa2db324bf86002476f01c90
Guy Bedford <gbedford@cloudflare.com>
2025-07-14 14:03:32 -0700

    regenerate types

--- types/generated-snapshot/latest/index.ts
+++ types/generated-snapshot/latest/index.ts
@@ -6491,13 +6491,13 @@ export interface IncomingRequestCfPropertiesBase
    *
    * @example 395747
    */
-  asn: number;
+  asn?: number;
   /**
    * The organization which owns the ASN of the incoming request.
    *
    * @example "Google Cloud"
    */
-  asOrganization: string;
+  asOrganization?: string;
   /**
    * The original value of the `Accept-Encoding` header if Cloudflare modified it.
    *
@@ -6624,7 +6624,7 @@ export interface IncomingRequestCfPropertiesCloudflareForSaaSEnterprise<
    * This field is only present if you have Cloudflare for SaaS enabled on your account
    * and you have followed the [required steps to enable it]((https://developers.cloudflare.com/cloudflare-for-platforms/cloudflare-for-saas/domain-support/custom-metadata/)).
    */
-  hostMetadata: HostMetadata;
+  hostMetadata?: HostMetadata;
 }
 export interface IncomingRequestCfPropertiesCloudflareAccessOrApiShield {
   /**
