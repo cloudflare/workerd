9e9c66d71d8dfb23d73b3627b30ee9fb92ccf0e0
Hood Chatham <roberthoodchatham@gmail.com>
2025-07-22 17:03:33 +0200

    Add make_snapshots script to automate generation of Python memory snapshots

--- src/pyodide/make_snapshots.py
+++ src/pyodide/make_snapshots.py
@@ -1,14 +1,8 @@
-import shutil
-import subprocess
-import sys
-from contextlib import contextmanager
-from hashlib import file_digest
 from pathlib import Path
+import subprocess
 from tempfile import TemporaryDirectory
-from textwrap import indent
-from time import time
 
-TEMPLATE = """
+CONFIG_TEMPLATE = """\
 using Workerd = import "/workerd/workerd.capnp";
 
 const config :Workerd.Config = (
@@ -19,166 +13,78 @@ const config :Workerd.Config = (
 
 const mainWorker :Workerd.Worker = (
   modules = [
-    (name = "worker.py", pythonModule = embed "./worker.py"),
-    {requirements}
+    {modules}
   ],
-  compatibilityDate = "2023-12-18",
-  compatibilityFlags = ["python_workers", {compat_flags}],
-  # Learn more about compatibility dates at:
-  # https://developers.cloudflare.com/workers/platform/compatibility-dates/
+  compatibilityDate = "{date}",
+  compatibilityFlags = [{flags}],
 );
 """
 
+PY_TEMPLATE = """\
+{imports}
 
-def make_config(
-    flags: list[str],
-    reqs: list[str],
-) -> str:
-    requirements = ""
-    for name in reqs:
-        requirements += f'(name="{name}", pythonRequirement=""),'
+def test():
+    pass
+"""
 
-    compat_flags = ""
-    for flag in flags:
-        compat_flags += f'"{flag}", '
-    return TEMPLATE.format(requirements=requirements, compat_flags=compat_flags)
+def run(args):
+    print(" ".join(str(x) for x in args))
+    subprocess.run(args)
 
 
-def make_worker(imports: list[str]) -> str:
-    contents = ""
-    for i in imports:
-        contents += f"import {i}\n"
-    return contents
+def make_config(outpath: Path, date: str, flags: list[str], packages: list[str], imports: list[str]):
+    import_fmted = "\n".join("import " + i for i in imports)
+    pyfile = PY_TEMPLATE.format(imports=import_fmted)
+    # escape new lines
+    pyfile = pyfile.replace("\n", "\\n")
 
+    main_module = f'(name = "worker.py", pythonModule = "{pyfile}")'
+    package_req = '(name = "{name}", pythonRequirement = "")'
+    modules_list = [main_module, *(package_req.format(name=name) for name in packages)]
+    modules = ",\n    ".join(modules_list)
 
-def run(cmd: list[str | Path]):
-    res = subprocess.run(
-        cmd,
-        capture_output=True,
-        text=True,
-        check=False,
-    )
-    if res.returncode:
-        print("Invocation failed:")
-        print(res.stdout)
-        print(res.stderr)
-        sys.exit(res.returncode)
+    flags_str = ", ".join(f'"{flag}"' for flag in flags)
+    outpath.write_text(CONFIG_TEMPLATE.format(pyfile=pyfile, modules=modules, date=date, flags=flags_str))
 
+def make_snapshot(date: str, flags: list[str], packages: list[str], imports: list[str], baseline: bool):
+    with TemporaryDirectory(delete=False) as d_str:
+        d = Path(d_str)
+        print(d)
+        config_path = d / "config.capnp"
+        make_config(config_path, date, flags, packages, imports)
+        if baseline:
+            snapshot_flag = "--python-save-baseline-snapshot"
+        else:
+            snapshot_flag = "--python-save-snapshot"
 
-def make_snapshot(  # noqa: PLR0913
-    d: Path,
-    outdir: Path,
-    outprefix: str,
-    compat_flags: list[str],
-    requirements: list[str],
-    imports: list[str],
-) -> str:
-    config_path = d / "config.capnp"
-    config_path.write_text(make_config(compat_flags, requirements))
-    worker_path = d / "worker.py"
-    worker_path.write_text(make_worker(imports))
-    run(
-        [
+        run([
             "bazel",
             "run",
             "@workerd//src/workerd/server:workerd",
             "--",
             "test",
             config_path,
-            "--python-save-snapshot",
+            snapshot_flag,
             "--pyodide-bundle-disk-cache-dir",
             d,
             "--pyodide-package-disk-cache-dir",
             d,
-            "--experimental",
-        ],
-    )
-    snapshot_path = d / "snapshot.bin"
-    digest = hexdigest(snapshot_path)
-    digest9 = digest[:9]
-    outname = f"{outprefix}-{digest9}.bin"
-    outfile = outdir / outname
-    shutil.copyfile(snapshot_path, outfile)
-    snapshot_path.unlink()
-    return [outname, digest]
+            "--experimental"
+        ])
 
 
-def bytesdigest(p: Path | str) -> bytes:
-    with Path(p).open("rb") as f:
-        return file_digest(f, "sha256").digest()
+def make_package_snapshot(date: str, flags: list[str], packages: list[str], imports: list[str]):
+    make_snapshot(date, flags, packages, imports, False)
 
-
-def hexdigest(p: Path | str) -> str:
-    digest = bytesdigest(p)
-    return "".join(hex(e)[2:] for e in digest)
-
-
-@contextmanager
-def timing(name: str):
-    print(f"Making {name}...")
-    start = time()
-    yield
-    elapsed = time() - start
-    print(f"... made {name} in {elapsed:.2f} seconds")
-
-
-def make_baseline_snapshot(
-    cache: Path, outdir: Path, compat_flags: list[str]
-) -> list[tuple[str, str]]:
-    name, digest = make_snapshot(cache, outdir, "baseline", compat_flags, [], [])
-    return [
-        ("baseline_snapshot", name),
-        ("baseline_snapshot_hash", digest),
-    ]
-
-
-def make_numpy_snapshot(
-    cache: Path, outdir: Path, compat_flags: list[str]
-) -> list[tuple[str, str]]:
-    name, digest = make_snapshot(
-        cache, outdir, "package_snapshot_numpy", compat_flags, ["numpy"], ["numpy"]
-    )
-    return [
-        ("numpy_snapshot", name),
-        ("numpy_snapshot_hash", digest),
-    ]
-
-
-def make_fastapi_snapshot(
-    cache: Path, outdir: Path, compat_flags: list[str]
-) -> list[tuple[str, str]]:
-    name, digest = make_snapshot(
-        cache,
-        outdir,
-        "package_snapshot_fastapi",
-        compat_flags,
-        ["fastapi"],
-        ["fastapi", "pydantic"],
-    )
-    return [
-        ("fastapi_snapshot", name),
-        ("fastapi_snapshot_hash", digest),
-    ]
-
-
-def main() -> int:
-    compat_flags = ["python_workers_20250116"]
-    with TemporaryDirectory() as package_cache:
-        cache = Path(package_cache)
-        cwd = Path.cwd()
-        res = []
-        with timing("baseline snapshot"):
-            res += make_baseline_snapshot(cache, cwd, compat_flags)
-        with timing("numpy snapshot"):
-            res += make_numpy_snapshot(cache, cwd, compat_flags)
-        with timing("fastapi snapshot"):
-            res += make_fastapi_snapshot(cache, cwd, compat_flags)
-    print()
-    print("Update python_metadata.bzl:\n")
-    for key, val in res:
-        print(indent(f'"{key}": "{val}"', " " * 8))
-    return 0
+def make_baseline_snapshot(date: str, flags: list[str]):
+    make_snapshot(date, flags, [], [], True)
 
 
 if __name__ == "__main__":
-    sys.exit(main())
+    date = "2023-12-18"
+    flags = ["python_workers", "python_workers_20250116"]
+    packages = ["numpy"]
+    imports = "numpy"
+    make_baseline_snapshot(date, flags)
+    make_package_snapshot(date, flags, packages, imports)
+
