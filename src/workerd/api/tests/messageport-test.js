5e7f10d38d3bbd23e6ceb5c4a29b7b6d7e1c8bd1
James M Snell <jsnell@cloudflare.com>
2025-07-22 14:42:44 -0700

    Clarify WPT testing status for MessageChannel/MessagePort

--- src/workerd/api/tests/messageport-test.js
+++ src/workerd/api/tests/messageport-test.js
@@ -12,6 +12,7 @@ export const simple1 = {
     const { promise, resolve } = Promise.withResolvers();
     const handler = mock.fn((event) => {
       strictEqual(event.data, 1);
+      strictEqual(event.isTrusted, true);
       resolve();
     });
     port2.onmessage = handler;
@@ -95,3 +96,40 @@ export const simple5 = {
     strictEqual(handler.mock.callCount(), 2);
   },
 };
+
+// The following are a selected subset of web platform tests for MessageChannel and MessagePort
+// that we know we pass. We don't support the full MessagePort spec so we're not going to run
+// the full WPT's for these yet.
+// Refs: https://github.com/web-platform-tests/wpt/blob/master/webmessaging/Channel_postMessage_Blob.any.js
+// Refs: https://github.com/web-platform-tests/wpt/blob/master/webmessaging/Channel_postMessage_DataCloneErr.any.js
+
+export const postMessageBlob = {
+  test() {
+    // Per the spec, Blob is a serializable object, but we don't implement it as such currently.
+    // So attempting to post a blob should throw an error.
+    const { port1 } = new MessageChannel();
+    throws(() => port1.postMessage(new Blob([''])), {
+      code: 25, // DATA_CLONE_ERR,
+      name: 'DataCloneError',
+      message: /Could not serialize/,
+    });
+  },
+};
+
+// Subset of the Web Platform Tests we know we don't pass, listed for future reference:
+// Most the web messaging WPT's are set up to require a full implementation of MessagePort
+// with web workers and most of the tests are in html files. We'll come back to these
+// later.
+// * https://github.com/web-platform-tests/wpt/blob/master/webmessaging/Channel_postMessage_clone_port.any.js
+// * https://github.com/web-platform-tests/wpt/blob/master/webmessaging/Channel_postMessage_clone_port_error.any.js
+// * https://github.com/web-platform-tests/wpt/blob/master/webmessaging/Channel_postMessage_ports_readonly_array.any.js
+// * https://github.com/web-platform-tests/wpt/blob/master/webmessaging/Channel_postMessage_transfer_xsite_incoming_messages.window.js
+// * https://github.com/web-platform-tests/wpt/blob/master/webmessaging/Channel_postMessage_transfer_xsite_incoming_messages.window.js
+// * https://github.com/web-platform-tests/wpt/blob/master/webmessaging/Channel_postMessage_with_transfer_incoming_messages.any.js
+// * https://github.com/web-platform-tests/wpt/blob/master/webmessaging/Channel_postMessage_with_transfer_outgoing_messages.any.js
+
+// This one is a bit special, per the spec we're supposed to fire off the close event
+// on an entangled port when the other port is garbage collected. We don't do that yet
+// and we might not ever. Need to investigate this further but it's not blocking us
+// right now.
+// * https://github.com/web-platform-tests/wpt/blob/master/webmessaging/message-channels/close-event/garbage-collected.tentative.any.js
