d634e4c3140e053bc2aef0965b9dd6db07aec5d9
Guy Bedford <gbedford@cloudflare.com>
2025-07-24 15:59:00 -0700

    extend hrtime even more (#4610)

--- src/workerd/api/node/tests/process-nodejs-test.js
+++ src/workerd/api/node/tests/process-nodejs-test.js
@@ -607,7 +607,7 @@ export const processHrtime = {
       // Ideally, this should be closer to 1000, or we could test
       // a smaller interval, but this is to work around the
       // test runner time accuracy.
-      assert(end - start >= 10_000_000n);
+      assert(end - start >= 1_000_000n);
     }
   },
 };
