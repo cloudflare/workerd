519e42aa6450ba018ae2896e5712eb73b7d23828
James M Snell <jsnell@cloudflare.com>
2025-08-09 07:54:05 -0700

    Small improvement to the httpServerHandler ergonomics
    Since calling `listen()` on the node:http Server is largely
    boilerplate, we can simplify the ergonomics by allowing the
    user to pass the server directly into httpServerHandler and
    having it call `listen()` for the user if it hasn't already
    been called.
    
    ```js
    import http from 'node:http';
    import { httpServerHandler } from 'cloudflare:node';
    
    const server = http.createServer((req, res) => {
      res.end('hello world');
    });
    
    export default httpServerHandler(server);
    ```

--- src/cloudflare/node.ts
+++ src/cloudflare/node.ts
@@ -3,23 +3,74 @@
 //     https://opensource.org/licenses/Apache-2.0
 import { portMapper } from 'cloudflare-internal:http';
 
+interface ServerDescriptor {
+  port?: number | null | undefined;
+}
+
+interface NodeStyleServer {
+  listen(...args: unknown[]): this;
+  address(): { port?: number | null | undefined };
+}
+
 export function httpServerHandler(
-  { port }: { port?: number } = {},
-  handlers: Record<string, unknown> = {}
+  desc: ServerDescriptor | NodeStyleServer,
+  handlers?: Record<string, unknown> | null
 ): {
   fetch(request: Request): Promise<Response>;
 } {
-  if (port == null) {
-    throw new Error('Port is required when calling httpServerHandler()');
-  }
+  // While the TypeScript type system prevents `desc` from being null or undefined,
+  // JavaScript does not, so we need to check for it at runtime.
   // eslint-disable-next-line @typescript-eslint/no-unnecessary-condition
-  if (handlers == null || typeof handlers !== 'object') {
+  if (desc == null) {
+    throw new Error('Server descriptor cannot be null or undefined');
+  }
+
+  // The desc can be a ServerDescriptor or a Server (where "Server" is defined
+  // as a type that has a "listen()" method and an "address()" method).
+  let port: number | null = null;
+
+  // If there is no port defined and desc has a listen method, we will try to
+  // access it as a Server, calling listen() if necessary to determine the port.
+  if (
+    (desc as ServerDescriptor).port == null &&
+    typeof (desc as NodeStyleServer).listen === 'function'
+  ) {
+    const server = desc as NodeStyleServer;
+    // First, let's see if the server-like thing already has a port.
+    // eslint-disable-next-line @typescript-eslint/no-unnecessary-condition
+    let serverPort = server.address()?.port;
+    if (typeof serverPort === 'number') {
+      // Nice, we're already bound to a port.
+      port = serverPort;
+    } else {
+      // We're not yet bound to a port. Try calling listen() to start the server
+      // and determine the port.
+      server.listen();
+      // Did it work? We do expect the listen in this case to synchronously
+      // assign the port so we can check it immediately.
+      // eslint-disable-next-line @typescript-eslint/no-unnecessary-condition
+      serverPort = server.address()?.port;
+      if (typeof serverPort === 'number') {
+        // Nice. We've got a port now.
+        port = serverPort;
+      }
+    }
+  } else if (typeof (desc as ServerDescriptor).port === 'number') {
+    // We're a ServerDescriptor with a port defined.
+    port = (desc as ServerDescriptor).port as number;
+  }
+
+  if (port == null) {
+    throw new Error('Failed to determine port for server');
+  }
+
+  if (handlers !== undefined && typeof handlers !== 'object') {
     throw new Error(
       'Handlers parameter passed to httpServerHandler method must be an object'
     );
   }
 
-  return Object.assign(handlers, {
+  return Object.assign(handlers ?? {}, {
     // We intentionally omitted ctx and env variables. Users should use
     // importable equivalents to access those values. For example, using
     // import { env, waitUntil } from 'cloudflare:workers
