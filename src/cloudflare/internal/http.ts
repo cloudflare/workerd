c612c54524a024ce2e43ca50acc7c69db14b8d5b
James M Snell <jsnell@cloudflare.com>
2025-08-12 10:07:13 -0700

    Rename Fetcher to FetchHandler
    This is the intended meaning of this interface defined in this file, but `Fetcher` commonly refers to the client-side interface, which led to confusion.
    
    Based on (and supersedes) James' PR #4761

--- src/cloudflare/internal/http.ts
+++ src/cloudflare/internal/http.ts
@@ -1,6 +1,7 @@
-export interface Fetcher {
+export interface FetchHandler {
   // We don't use typeof fetch here because that would use the client-side signature,
   // which is different from the server-side signature.
   fetch: (request: Request, env?: unknown, ctx?: unknown) => Promise<Response>;
 }
-export const portMapper = new Map<number, Fetcher>();
+
+export const portMapper = new Map<number, FetchHandler>();
