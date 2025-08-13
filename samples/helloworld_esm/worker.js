d75f049a0f11d41698df55fc11860d71791aae06
James M Snell <jsnell@cloudflare.com>
2025-08-13 05:23:46 -0700

    Revert "Revert "Use new js error serialization to preserve error details (#4698)" (#4717)"
    This reverts commit d218ae6c3d6402d44e3404f3f74f7c5874a157ad.

--- samples/helloworld_esm/worker.js
+++ samples/helloworld_esm/worker.js
@@ -2,6 +2,8 @@
 // Licensed under the Apache 2.0 license found in the LICENSE file or at:
 //     https://opensource.org/licenses/Apache-2.0
 
+throw new AggregateError([new Error('boom')], 'message', { cause: new Error('cause') });
+
 export default {
   async fetch(req, env) {
     return new Response("Hello World\n");
