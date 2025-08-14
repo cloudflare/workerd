3027443d17304a04b9ca94417fccef92a3d3f4d6
Caio Nogueira <caio@cloudflare.com>
2025-08-14 13:37:26 +0100

    repro for stack leaking bug

--- src/workerd/api/tests/rpc-error-test.rpc.js
+++ src/workerd/api/tests/rpc-error-test.rpc.js
@@ -1,42 +1,48 @@
-import { RpcTarget } from 'cloudflare:workers';
+import { DurableObject } from 'cloudflare:workers';
 
-class Baz extends RpcTarget {
-  async qux() {
+// class Baz extends RpcTarget {
+//   async qux() {
+//     await scheduler.wait(10);
+//     const err = new Error('bork');
+//     err.stack2 = err.stack;
+//     throw err;
+//   }
+// }
+
+export class Bar extends DurableObject {
+  async bar() {
+    await this.blah()
+  }
+
+  async blah () {
     await scheduler.wait(10);
     const err = new Error('bork');
-    err.stack2 = err.stack;
+    err.trusted = true
     throw err;
   }
 }
 
-class Bar extends RpcTarget {
-  async baz() {
-    await scheduler.wait(10);
-    return new Baz();
-  }
-}
+// export default {
+//   foo() {
+//     const err = new Error('boom');
+//     err.stack2 = err.stack;
+//     throw err;
+//   },
 
-export default {
-  foo() {
-    const err = new Error('boom');
-    err.stack2 = err.stack;
-    throw err;
-  },
+//   async bar() {
+//     await scheduler.wait(10);
+//     return new Bar();
+//   },
 
-  async bar() {
-    await scheduler.wait(10);
-    return new Bar();
-  },
+//   xyz() {
+//     const err = new Error('bang');
+//     err.stack2 = err.stack;
+//     return Promise.reject(err);
+//   },
 
-  xyz() {
-    const err = new Error('bang');
-    err.stack2 = err.stack;
-    return Promise.reject(err);
-  },
-
-  abc() {
-    const err = new Error('boom');
-    err.stack2 = err.stack;
-    return { a: err };
-  },
-};
+//   abc() {
+//     const err = new Error('boom');
+//     err.stack2 = err.stack;
+//     return { a: err };
+//   },
+// };
