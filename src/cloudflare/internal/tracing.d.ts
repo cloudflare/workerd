d42c0d2038d94184ad05ae06ba38e9116f113e0b
mar-cf <mar@cloudflare.com>
2025-09-30 01:22:49 -0500

    Remove unnecessary methods and cleanups.

--- src/cloudflare/internal/tracing.d.ts
+++ src/cloudflare/internal/tracing.d.ts
@@ -2,8 +2,6 @@ export class JsSpanBuilder {
   setTag(key: string, value: string | number | boolean | undefined): void;
   // Closes the span
   end(): void;
-  // Returns whether this span is actively recording (not a no-op)
-  getIsRecording(): boolean;
 }
 
 function startSpan(name: string): JsSpanBuilder;
