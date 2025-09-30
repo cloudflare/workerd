5ddb501c4c1325237c72b3498099203377e92410
mar-cf <mar@cloudflare.com>
2025-09-30 02:47:27 -0500

    Add comments.

--- src/cloudflare/internal/tracing.d.ts
+++ src/cloudflare/internal/tracing.d.ts
@@ -1,4 +1,5 @@
 export class Span {
+  // Sets a tag on this span. If value is undefined, the tag is not set.
   setTag(key: string, value: string | number | boolean | undefined): void;
   // Closes the span
   end(): void;
