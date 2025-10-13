f6172166ea5b79dff94c4a34bcd5e9e16657ab55
mar-cf <mar@cloudflare.com>
2025-10-13 14:38:32 -0500

    Add tracing infrastructure for bindings
    Co-authored-by: Jeremy Morrell (Cloudflare) <jmorrell@cloudflare.com>
    Co-authored-by: Zeb Piasecki <zeb@cloudflare.com>
    Co-authored-by: Maksym Makuch <maksymm@protonmail.com>

--- src/workerd/api/modules.h
+++ src/workerd/api/modules.h
@@ -10,6 +10,7 @@
 #include <workerd/api/pyodide/pyodide.h>
 #include <workerd/api/rtti.h>
 #include <workerd/api/sockets.h>
+#include <workerd/api/tracing-module.h>
 #include <workerd/api/unsafe.h>
 #include <workerd/api/workers-module.h>
 #include <workerd/jsg/modules-new.h>
@@ -52,6 +53,7 @@ void registerModules(Registry& registry, auto featureFlags) {
   registerBase64Module(registry, featureFlags);
   registry.addBuiltinBundle(CLOUDFLARE_BUNDLE);
   registerWorkersModule(registry, featureFlags);
+  registerTracingModule(registry, featureFlags);
   registry.template addBuiltinModule<EnvModule>(
       "cloudflare-internal:env", workerd::jsg::ModuleRegistry::Type::INTERNAL);
   registry.template addBuiltinModule<FileSystemModule>(
@@ -67,6 +69,7 @@ void registerBuiltinModules(jsg::modules::ModuleRegistry::Builder& builder, auto
   builder.add(getInternalRpcModuleBundle<TypeWrapper>(featureFlags));
 
   builder.add(getInternalUnsafeModuleBundle<TypeWrapper>(featureFlags));
+  builder.add(getInternalTracingModuleBundle<TypeWrapper>(featureFlags));
   if (featureFlags.getUnsafeModule()) {
     builder.add(getExternalUnsafeModuleBundle<TypeWrapper>(featureFlags));
   }
