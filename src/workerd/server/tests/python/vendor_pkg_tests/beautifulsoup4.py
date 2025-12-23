a6b24d8f51edd47414aac05e35960907e6ee6f93
Dan Lapid <dan.lapid@gmail.com>
2025-12-23 23:11:45 +0000

    Make all tests pass after adding all-compat-flags variant
    First we bulk remove compat date from all tests.
    Then we add enable-flags to make the default variant pass.
    Then we add disable-flags to make the all-compat-flags variant pass.
    
    Python tests don't run all-compat-flags and all-autogates variants.
    I made them all pass under those variants and then disabled the variants.
    The impact on CI times is just way too high to justify the value sadly.
    We need to find a way to improve python test CI times and then reenable them.

--- src/workerd/server/tests/python/vendor_pkg_tests/beautifulsoup4.py
+++ src/workerd/server/tests/python/vendor_pkg_tests/beautifulsoup4.py
@@ -1,9 +1,12 @@
 import bs4
+from workers import WorkerEntrypoint
 
 
-async def test():
-    assert bs4.__version__
-    # Use beautifulsoup4 to parse HTML
-    soup = bs4.BeautifulSoup("<html></html>", "html.parser")
-    print(soup.prettify())
-    print("BeautifulSoup4 ran successfully!")
+class Default(WorkerEntrypoint):
+    async def test(self):
+        assert bs4.__version__
+        # Use beautifulsoup4 to parse HTML
+        soup = bs4.BeautifulSoup("<html></html>", "html.parser")
+        print(soup.prettify())
+
+        print("BeautifulSoup4 ran successfully!")
--- src/workerd/server/tests/python/vendor_pkg_tests/python-workers-runtime-sdk.py
+++ src/workerd/server/tests/python/vendor_pkg_tests/python-workers-runtime-sdk.py
@@ -1,9 +1,10 @@
 # The python-workers-runtime-sdk vendored files include the sdk in tree from workerd
 # 9ca3b8e36e86e34f16ecd9680334ba0e0261549c plus one extra function. If we don't at least have the
 # things imported in introspection.py we'll crash on startup.
-from workers import extra_function
+from workers import WorkerEntrypoint, extra_function
 
 
-async def test():
-    assert extra_function() == 42
-    print("workers-sdk imported successfully!")
+class Default(WorkerEntrypoint):
+    async def test(self):
+        assert extra_function() == 42
+        print("workers-sdk imported successfully!")
