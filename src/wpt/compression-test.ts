4ccae537bbc8621c13cfa66e51f8928955fab65a
Yagiz Nizipli <yagiz@nizipli.com>
2026-01-06 12:03:52 -0500

    fix web-platform tests web-idl parser (#5830)

--- src/wpt/compression-test.ts
+++ src/wpt/compression-test.ts
@@ -53,8 +53,26 @@ export default {
     ],
   },
   'idlharness.https.any.js': {
-    comment: 'Test file /resources/WebIDLParser.js not found',
-    disabledTests: true,
+    comment:
+      'Workers expose globals differently than browsers - these interface tests fail',
+    expectedFailures: [
+      'CompressionStream interface: existence and properties of interface object',
+      'CompressionStream interface object length',
+      'CompressionStream interface object name',
+      'CompressionStream interface: existence and properties of interface prototype object',
+      'CompressionStream interface: existence and properties of interface prototype object\'s "constructor" property',
+      "CompressionStream interface: existence and properties of interface prototype object's @@unscopables property",
+      'CompressionStream must be primary interface of new CompressionStream("deflate")',
+      'Stringification of new CompressionStream("deflate")',
+      'DecompressionStream interface: existence and properties of interface object',
+      'DecompressionStream interface object length',
+      'DecompressionStream interface object name',
+      'DecompressionStream interface: existence and properties of interface prototype object',
+      'DecompressionStream interface: existence and properties of interface prototype object\'s "constructor" property',
+      "DecompressionStream interface: existence and properties of interface prototype object's @@unscopables property",
+      'DecompressionStream must be primary interface of new DecompressionStream("deflate")',
+      'Stringification of new DecompressionStream("deflate")',
+    ],
   },
   'third_party/pako/pako_inflate.min.js': {},
 } satisfies TestRunnerConfig;
