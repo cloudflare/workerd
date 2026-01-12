e4ba6a3c141e7f09700856736ee32f516bcbe06c
Victor Berchet <victor@suumit.com>
2026-01-12 16:02:50 +0100

    test mutable import

---
+++ src/workerd/api/node/tests/module-import-mutable-test.js
@@ -0,0 +1,25 @@
+// Copyright (c) 2017-2026 Cloudflare, Inc.
+// Licensed under the Apache 2.0 license found in the LICENSE file or at:
+//     https://opensource.org/licenses/Apache-2.0
+import { strictEqual, ok, doesNotThrow } from 'node:assert';
+
+export const testTimersPromisesMutable = {
+  async test() {
+    const timersPromises = await import('node:timers/promises');
+    const originalSetImmediate = timersPromises.setImmediate;
+    ok(typeof originalSetImmediate === 'function');
+
+    const patchedSetImmediate = async function patchedSetImmediate() {
+      return 'patched';
+    };
+
+    doesNotThrow(() => {
+      timersPromises.setImmediate = patchedSetImmediate;
+    });
+
+    strictEqual(timersPromises.setImmediate, patchedSetImmediate);
+    strictEqual(await timersPromises.setImmediate(), 'patched');
+    timersPromises.setImmediate = originalSetImmediate;
+    strictEqual(timersPromises.setImmediate, originalSetImmediate);
+  },
+};
