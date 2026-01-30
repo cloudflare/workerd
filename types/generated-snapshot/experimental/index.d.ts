144e83785740e4778064216c3ae65a8c11393f01
Olga Silva <osilva@cloudflare.com>
2026-01-30 14:31:53 +0000

    [Workflows] Add WorkflowsInstanceStatus

--- types/generated-snapshot/experimental/index.d.ts
+++ types/generated-snapshot/experimental/index.d.ts
@@ -12239,6 +12239,16 @@ declare namespace CloudflareWorkersModule {
       },
     ): Promise<WorkflowStepEvent<T>>;
   }
+  export type WorkflowInstanceStatus =
+    | "queued"
+    | "running"
+    | "paused"
+    | "errored"
+    | "terminated"
+    | "complete"
+    | "waiting"
+    | "waitingForPause"
+    | "unknown";
   export abstract class WorkflowEntrypoint<
     Env = unknown,
     T extends Rpc.Serializable<T> | unknown = unknown,
