f584a3768b36c63a9e6790969a1bcf31e135dd05
Florent Collin <fcollin@cloudflare.com>
2026-02-09 13:12:59 +0000

    CFSQL-1473: Remove meta from D1UpstreamFailure

--- src/cloudflare/internal/d1-api.ts
+++ src/cloudflare/internal/d1-api.ts
@@ -60,7 +60,7 @@ type D1UpstreamFailure = {
   results?: never;
   error: string;
   success: false;
-  meta: D1Meta & Record<string, unknown>;
+  meta?: never;
 };
 
 type D1RowsColumns<T = unknown> = D1Response & {
@@ -386,39 +386,42 @@ class D1DatabaseSessionAlwaysPrimary extends D1DatabaseSession {
       span.setAttribute('db.query.text', query);
       span.setAttribute('cloudflare.binding.type', 'D1');
 
+      // TODO: splitting by lines is overly simplification because a single line
+      // can contain multiple statements (ex: `select 1; select 2;`).
+      // Also, a statement can span multiple lines...
+      // Either, we should do a more reasonable job to split the query into multiple statements
+      // like we do in the D1 codebase or we report a simpler error without the line number.
       const lines = query.trim().split('\n');
       const _exec = await this._send('/execute', lines, [], 'NONE', span);
       const exec = Array.isArray(_exec) ? _exec : [_exec];
 
-      const error = exec
-        .map((r) => {
-          return r.error ? 1 : 0;
-        })
-        .indexOf(1);
-      if (error !== -1) {
-        span.setAttribute('error.type', `Error in line ${error + 1}`);
-        throw new Error(
-          `D1_EXEC_ERROR: Error in line ${error + 1}: ${lines[error]}: ${
-            exec[error]?.error
-          }`,
-          {
-            cause: new Error(
-              `Error in line ${error + 1}: ${lines[error]}: ${exec[error]?.error}`
-            ),
-          }
-        );
-      } else {
-        addAggregatedD1MetaToSpan(
-          span,
-          exec.map((e) => e.meta)
-        );
-        return {
-          count: exec.length,
-          duration: exec.reduce((p, c) => {
-            return p + c.meta['duration'];
-          }, 0),
-        };
+      let duration = 0;
+      const metas: D1Meta[] = [];
+      for (let i = 0; i < exec.length; i++) {
+        const res = exec[i];
+        if (!res?.success) {
+          span.setAttribute('error.type', `Error in line ${i + 1}`);
+          throw new Error(
+            `D1_EXEC_ERROR: Error in line ${i + 1}: ${lines[i]}${res?.error ? `: ${res.error}` : ''}`,
+            {
+              cause: new Error(
+                `Error in line ${i + 1}: ${lines[i]}${res?.error ? `: ${res.error}` : ''}`
+              ),
+            }
+          );
+        }
+
+        duration += res.meta.duration;
+        metas.push(res.meta);
       }
+
+      if (metas.length) {
+        addAggregatedD1MetaToSpan(span, metas);
+      }
+      return {
+        count: exec.length,
+        duration,
+      };
     });
   }
 
@@ -711,12 +714,11 @@ function mapD1Result<T>(result: D1UpstreamResponse<T>): D1UpstreamResponse<T> {
   return result.error
     ? {
         success: false,
-        meta: result.meta,
         error: result.error,
       }
     : {
         success: true,
-        meta: result.meta,
+        meta: (result as D1UpstreamSuccess).meta,
         ...('results' in result ? { results: result.results } : {}),
       };
 }
@@ -730,9 +732,10 @@ async function toJson<T = unknown>(response: Response): Promise<T> {
   }
 }
 
-function addAggregatedD1MetaToSpan(span: Span, metas: D1Meta[]): void {
-  // eslint-disable-next-line @typescript-eslint/no-unnecessary-condition
-  if (!metas?.length) {
+type PartialD1Meta = Partial<D1Meta> | undefined;
+
+function addAggregatedD1MetaToSpan(span: Span, metas: PartialD1Meta[]): void {
+  if (!metas.length) {
     return;
   }
   const aggregatedMeta = aggregateD1Meta(metas);
@@ -767,7 +770,7 @@ function addD1MetaToSpan(span: Span, meta: D1Meta): void {
 // When a query is executing multiple statements, and we receive a D1Meta
 // for each statement, we need to aggregate the meta data before we annotate
 // the telemetry, with different rules for each field.
-function aggregateD1Meta(metas: D1Meta[]): D1Meta {
+function aggregateD1Meta(metas: PartialD1Meta[]): D1Meta {
   const aggregatedMeta: D1Meta = {
     duration: 0,
     size_after: 0,
@@ -779,21 +782,15 @@ function aggregateD1Meta(metas: D1Meta[]): D1Meta {
   };
 
   for (const meta of metas) {
-    // eslint-disable-next-line @typescript-eslint/no-unnecessary-condition
     if (!meta) {
       continue;
     }
 
-    // eslint-disable-next-line @typescript-eslint/no-unnecessary-condition
     aggregatedMeta.duration += meta.duration ?? 0;
     // for size_after, we only want the last value
-    // eslint-disable-next-line @typescript-eslint/no-unnecessary-condition
     aggregatedMeta.size_after = meta.size_after ?? 0;
-    // eslint-disable-next-line @typescript-eslint/no-unnecessary-condition
     aggregatedMeta.rows_read += meta.rows_read ?? 0;
-    // eslint-disable-next-line @typescript-eslint/no-unnecessary-condition
     aggregatedMeta.rows_written += meta.rows_written ?? 0;
-    // eslint-disable-next-line @typescript-eslint/no-unnecessary-condition
     aggregatedMeta.last_row_id = meta.last_row_id ?? 0;
     if (meta.served_by_region) {
       aggregatedMeta.served_by_region = meta.served_by_region;
@@ -812,7 +809,6 @@ function aggregateD1Meta(metas: D1Meta[]): D1Meta {
       aggregatedMeta.total_attempts =
         (aggregatedMeta.total_attempts ?? 0) + meta.total_attempts;
     }
-    // eslint-disable-next-line @typescript-eslint/no-unnecessary-condition
     aggregatedMeta.changes += meta.changes ?? 0;
     if (meta.changed_db) {
       aggregatedMeta.changed_db = true;
