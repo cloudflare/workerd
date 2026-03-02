f3d9ee57505d2b99c3757ef825daa9bd723f286b
Yagiz Nizipli <yagiz@nizipli.com>
2026-03-02 16:11:15 -0500

    fix EventTarget spec compliance testing

--- src/wpt/dom/events-test.ts
+++ src/wpt/dom/events-test.ts
@@ -47,10 +47,7 @@ export default {
   },
   'EventTarget-add-remove-listener.any.js': {},
   'EventTarget-addEventListener.any.js': {},
-  'EventTarget-constructible.any.js': {
-    comment: 'Should be null, not EventTarget',
-    expectedFailures: [],
-  },
+  'EventTarget-constructible.any.js': {},
   'EventTarget-removeEventListener.any.js': {
     comment: 'capture is not relevant outside of the DOM',
     expectedFailures: ['removing a null event listener should succeed'],
