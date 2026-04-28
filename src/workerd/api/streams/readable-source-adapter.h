6af6ecf7c942e2a062bc3f689e6e4d3e92feb47d
James M Snell <jsnell@cloudflare.com>
2026-04-27 17:02:02 -0700

    No jsg::BufferSource in readable-source-adapter

--- src/workerd/api/streams/readable-source-adapter.h
+++ src/workerd/api/streams/readable-source-adapter.h
@@ -159,7 +159,7 @@ class ReadableStreamSourceJsAdapter final {
     // is equal to the length of this buffer. The actual number of
     // bytes read is indicated by the resolved value of the promise
     // but will never exceed the length of this buffer.
-    jsg::BufferSource buffer;
+    jsg::JsRef<jsg::JsArrayBufferView> buffer;
 
     // The optional minimum number of bytes to read. If not provided,
     // the read will complete as soon as at least the mininum number
@@ -179,7 +179,7 @@ class ReadableStreamSourceJsAdapter final {
     // of the same type as that provided in ReadOptions.
     // If the read produced no data because the stream is
     // closed, the type array will be zero length.
-    jsg::BufferSource buffer;
+    jsg::JsRef<jsg::JsArrayBufferView> buffer;
 
     // True if the stream is now closed and no further reads
     // are possible. If this is true, the buffer will be zero
