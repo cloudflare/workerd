5d870963b24225370b8091d2306874d485dbbd3c
James M Snell <jsnell@cloudflare.com>
2026-06-03 16:08:44 -0700

    Optionally pass IoContext& ref into ioContext.run lambda
    When the `ioContext.run(fn)` lambda is passed, we often either
    explicitly capture a `&ioContext` in the lambda captures or end
    up calling `IoContext::current()` within the body. This is silly
    because the `fn` lambda wouldn't be called if the lambda didn't
    currently exist and we don't have to wonder what `IoContext`
    called it. However, we also don't necessarily want to go through
    and update every call site for `ioContext.run` at once.
    
    This commit makes it possible to accept an additional
    `IoContext&` argument in the run lambda...
    
    ```cpp
    ioContext.run([](jsg::Lock& js, IoContext& ioContext) {
      // ...
    });
    ```
    
    No need to capture it, no need to look it up with
    `IoContext::current()`. Existing call sites that do not
    specify the second argument can be updated incrementally
    over time.

--- src/workerd/io/io-context.h
+++ src/workerd/io/io-context.h
@@ -29,6 +29,8 @@
 #include <kj/function.h>
 #include <kj/mutex.h>
 
+#include <concepts>
+
 namespace workerd {
 class WorkerTracer;
 class BaseTracer;
@@ -377,15 +379,34 @@ class IoContext final: public kj::Refcounted, private kj::TaskSet::ErrorHandler 
   //
   // If `inputLock` is not provided, and this is an actor context, an input lock will be obtained
   // before executing the callback.
+  //
+  // The callback can accept either (Worker::Lock&) or (Worker::Lock&, IoContext&). When the
+  // two-argument form is used, *this is passed as the second argument. Existing single-argument
+  // call sites are unaffected.
   template <typename Func>
-  kj::PromiseForResult<Func, Worker::Lock&> run(
-      Func&& func, kj::Maybe<InputGate::Lock> inputLock = kj::none) KJ_WARN_UNUSED_RESULT;
+  auto run(Func&& func, kj::Maybe<InputGate::Lock> inputLock = kj::none) KJ_WARN_UNUSED_RESULT {
+    if constexpr (runFuncAcceptsIoContext<Func>) {
+      return runSingle([this, func = kj::fwd<Func>(func)](Worker::Lock& lock) mutable {
+        return func(lock, *this);
+      }, kj::mv(inputLock));
+    } else {
+      return runSingle(kj::fwd<Func>(func), kj::mv(inputLock));
+    }
+  }
 
   // Like run() but executes within the given critical section, if it is non-null. If
   // `criticalSection` is null, then this just forwards to the other run() (with null inputLock).
   template <typename Func>
-  kj::PromiseForResult<Func, Worker::Lock&> run(Func&& func,
-      kj::Maybe<kj::Own<InputGate::CriticalSection>> criticalSection) KJ_WARN_UNUSED_RESULT;
+  auto run(Func&& func,
+      kj::Maybe<kj::Own<InputGate::CriticalSection>> criticalSection) KJ_WARN_UNUSED_RESULT {
+    if constexpr (runFuncAcceptsIoContext<Func>) {
+      return runSingle([this, func = kj::fwd<Func>(func)](Worker::Lock& lock) mutable {
+        return func(lock, *this);
+      }, kj::mv(criticalSection));
+    } else {
+      return runSingle(kj::fwd<Func>(func), kj::mv(criticalSection));
+    }
+  }
 
   // Returns the current IoContext for the thread.
   // Throws an exception if there is no current context (see hasCurrent() below).
@@ -1097,6 +1118,20 @@ class IoContext final: public kj::Refcounted, private kj::TaskSet::ErrorHandler 
       kj::Maybe<InputGate::Lock> inputLock,
       Runnable::Exceptional exceptional);
 
+  // Detect whether a run() callback accepts IoContext& as a second argument.
+  template <typename Func>
+  static constexpr bool runFuncAcceptsIoContext =
+      std::invocable<std::decay_t<Func>&, Worker::Lock&, IoContext&>;
+
+  // Internal implementation of run(), always invoked with a single-arg callback.
+  template <typename Func>
+  kj::PromiseForResult<Func, Worker::Lock&> runSingle(
+      Func&& func, kj::Maybe<InputGate::Lock> inputLock = kj::none);
+
+  template <typename Func>
+  kj::PromiseForResult<Func, Worker::Lock&> runSingle(
+      Func&& func, kj::Maybe<kj::Own<InputGate::CriticalSection>> criticalSection);
+
   void abortFromHang(Worker::AsyncLock& asyncLock);
 
   template <typename T>
@@ -1185,21 +1220,21 @@ kj::Promise<T> IoContext::lockOutputWhile(kj::Promise<T> promise) {
 }
 
 template <typename Func>
-kj::PromiseForResult<Func, Worker::Lock&> IoContext::run(
+kj::PromiseForResult<Func, Worker::Lock&> IoContext::runSingle(
     Func&& func, kj::Maybe<kj::Own<InputGate::CriticalSection>> criticalSection) {
   KJ_IF_SOME(cs, criticalSection) {
     return cs.get()
         ->wait(getCurrentTraceSpan())
         .then([this, func = kj::fwd<Func>(func)](InputGate::Lock&& inputLock) mutable {
-      return run(kj::fwd<Func>(func), kj::mv(inputLock));
+      return runSingle(kj::fwd<Func>(func), kj::mv(inputLock));
     });
   } else {
-    return run(kj::fwd<Func>(func));
+    return runSingle(kj::fwd<Func>(func));
   }
 }
 
 template <typename Func>
-kj::PromiseForResult<Func, Worker::Lock&> IoContext::run(
+kj::PromiseForResult<Func, Worker::Lock&> IoContext::runSingle(
     Func&& func, kj::Maybe<InputGate::Lock> inputLock) {
   // Before we try running anything, let's make sure our IoContext hasn't been aborted. If it has
   // been aborted, there's likely not an active request so later operations will fail anyway.
@@ -1213,7 +1248,7 @@ kj::PromiseForResult<Func, Worker::Lock&> IoContext::run(
       return a.getInputGate()
           .wait(getCurrentTraceSpan())
           .then([this, func = kj::fwd<Func>(func)](InputGate::Lock&& inputLock) mutable {
-        return run(kj::fwd<Func>(func), kj::mv(inputLock));
+        return runSingle(kj::fwd<Func>(func), kj::mv(inputLock));
       });
     }
 
