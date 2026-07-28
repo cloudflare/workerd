2ce5537726eb1474c22291016824425e88cd4663
Mike Aizatsky <maizatskyi@cloudflare.com>
2026-07-28 12:51:19 -0700

    cleanup unused fields

--- src/workerd/api/streams/readable.h
+++ src/workerd/api/streams/readable.h
@@ -80,7 +80,6 @@ private:
       Closed,
       Released>;
 
-  kj::WeakRc<IoContext> ioContext = IoContext::tryGetWeakRefForCurrent();
   ReadableStreamController::Reader& reader;
 
   ReaderState state;
@@ -258,7 +257,6 @@ class DrainingReader: public ReadableStreamController::Reader {
   using Attached = jsg::Ref<ReadableStream>;
   struct Released {};
 
-  kj::WeakRc<IoContext> ioContext = IoContext::tryGetWeakRefForCurrent();
   kj::OneOf<Initial, Attached, StreamStates::Closed, Released> state = Initial();
   kj::Maybe<jsg::MemoizedIdentity<jsg::Promise<void>>> closedPromise;
 };
@@ -476,7 +474,6 @@ public:
   void visitForMemoryInfo(jsg::MemoryTracker& tracker) const;
 
 private:
-  kj::WeakRc<IoContext> ioContext = IoContext::tryGetWeakRefForCurrent();
   kj::Own<ReadableStreamController> controller;
 
   // Used to signal when this ReadableStream reads EOF. This signal is required for TCP sockets.
