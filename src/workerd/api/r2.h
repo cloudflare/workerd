d9c59b5c0433e5d163474d4802a9a605a34e5a44
Carol Xu <cxu@cloudflare.com>
2026-08-21 17:19:50 -0500

    Add named method calls for delete and head and gate them

--- src/workerd/api/r2.h
+++ src/workerd/api/r2.h
@@ -17,6 +17,7 @@ namespace workerd::api::public_beta {
       api::public_beta::R2Bucket::Checksums, api::public_beta::R2Bucket::StringChecksums,          \
       api::public_beta::R2Bucket::HttpMetadata, api::public_beta::R2Bucket::ListOptions,           \
       api::public_beta::R2Bucket::ListResult,                                                      \
-      api::public_beta::R2MultipartUpload::UploadPartOptions
+      api::public_beta::R2MultipartUpload::UploadPartOptions,                                      \
+      api::public_beta::R2Bucket::ChecksumsRpc, api::public_beta::R2Bucket::HeadResultRpc
 // The list of r2 types that are added to worker.c++'s JSG_DECLARE_ISOLATE_TYPE
 }  // namespace workerd::api::public_beta
