5558e5b87a144cc7b7622250208675468a4a0138
James M Snell <jsnell@cloudflare.com>
2026-08-27 22:21:42 +0000

    Document the compression streams suite

--- src/tests/streams/compression/legacy-nonstrict.js
+++ src/tests/streams/compression/legacy-nonstrict.js
@@ -25,9 +25,9 @@ export const legacyTrailingDataTolerated = {
   async test() {
     // Gzipped "FOOBAR" plus a trailing 0xFF byte.
     const trailing = new Uint8Array([
-      0x1f, 0x8b, 0x08, 0x00, 0xf9, 0x05, 0xb7, 0x59, 0x00, 0x03, 0x4b,
-      0xcb, 0xcf, 0x4f, 0x4a, 0x2c, 0x02, 0x00, 0x95, 0x1f, 0xf6, 0x9e,
-      0x06, 0x00, 0x00, 0x00, 0xff,
+      0x1f, 0x8b, 0x08, 0x00, 0xf9, 0x05, 0xb7, 0x59, 0x00, 0x03, 0x4b, 0xcb,
+      0xcf, 0x4f, 0x4a, 0x2c, 0x02, 0x00, 0x95, 0x1f, 0xf6, 0x9e, 0x06, 0x00,
+      0x00, 0x00, 0xff,
     ]);
     const ds = new DecompressionStream('gzip');
     const writer = ds.writable.getWriter();
@@ -67,16 +67,19 @@ export const legacyInvalidChunkThrowsSynchronously = {
   async test() {
     const cs = new CompressionStream('deflate');
     const writer = cs.writable.getWriter();
-    throws(() => writer.write(42), (err) => {
-      strictEqual(err.constructor, TypeError);
-      strictEqual(
-        err.message,
-        'This TransformStream is being used as a byte stream, but received ' +
-          'an object of non-ArrayBuffer/ArrayBufferView type on its ' +
-          'writable side.'
-      );
-      return true;
-    });
+    throws(
+      () => writer.write(42),
+      (err) => {
+        strictEqual(err.constructor, TypeError);
+        strictEqual(
+          err.message,
+          'This TransformStream is being used as a byte stream, but received ' +
+            'an object of non-ArrayBuffer/ArrayBufferView type on its ' +
+            'writable side.'
+        );
+        return true;
+      }
+    );
     // The stream survives: later traffic flows.
     await writer.write(enc.encode('ok'));
     await writer.close();
