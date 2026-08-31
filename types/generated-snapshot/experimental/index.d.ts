0e03c2eb5bea0fc424342ea61d096da7f640bb7e
Thomas Rubini <trubini@cloudflare.com>
2026-08-31 18:11:32 +0000

    make UDP sockets use Datagram objects + handle TS streams

--- types/generated-snapshot/experimental/index.d.ts
+++ types/generated-snapshot/experimental/index.d.ts
@@ -382,6 +382,7 @@ interface ServiceWorkerGlobalScope extends WorkerGlobalScope {
   FixedLengthStream: typeof FixedLengthStream;
   IdentityTransformStream: typeof IdentityTransformStream;
   HTMLRewriter: typeof HTMLRewriter;
+  Datagram: typeof Datagram;
   Performance: typeof Performance;
   PerformanceEntry: typeof PerformanceEntry;
   PerformanceMark: typeof PerformanceMark;
@@ -3967,6 +3968,7 @@ interface Socket {
   get opened(): Promise<SocketInfo>;
   get upgraded(): boolean;
   get secureTransport(): "on" | "off" | "starttls";
+  get protocol(): "tcp" | "udp";
   close(): Promise<void>;
   startTls(options?: TlsOptions): Socket;
 }
@@ -3985,6 +3987,10 @@ interface TlsOptions {
 interface SocketInfo {
   remoteAddress?: string;
   localAddress?: string;
+}
+declare class Datagram {
+  constructor(data: Uint8Array);
+  get data(): Uint8Array;
 }
 /**
  * The **`EventSource`** interface is web content's interface to server-sent events.
