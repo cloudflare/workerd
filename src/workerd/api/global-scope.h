cfca331175ac370d3bc86c92e7512675c48c7842
Thomas Rubini <trubini@cloudflare.com>
2026-08-31 18:11:32 +0000

    make UDP sockets use Datagram objects + handle TS streams

--- src/workerd/api/global-scope.h
+++ src/workerd/api/global-scope.h
@@ -70,6 +70,7 @@ class TextDecoderStream;
 class Blob;
 class File;
 class FormData;
+class Datagram;
 
 class URLPattern;
 namespace urlpattern {
@@ -1033,6 +1034,7 @@ class ServiceWorkerGlobalScope: public WorkerGlobalScope {
     JSG_NESTED_TYPE(FixedLengthStream);
     JSG_NESTED_TYPE(IdentityTransformStream);
     JSG_NESTED_TYPE(HTMLRewriter);
+    JSG_NESTED_TYPE(Datagram);
 
     // Performance API
     if (flags.getEnableGlobalPerformanceClasses() || flags.getEnableNodeJsPerfHooksModule()) {
