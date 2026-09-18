62935d76771b2361e507c7f18f064c5bab43c314
Felix Hanau <felix@cloudflare.com>
2026-09-18 15:18:28 -0400

    [o11y] Add `undefined` to tailStream return types
    Returning undefined indicates that tail events for the given worker event can be
    ignored, which is used to shut down the tail stream early as an optimization.

--- types/generated-snapshot/experimental/index.ts
+++ types/generated-snapshot/experimental/index.ts
@@ -534,7 +534,10 @@ export type ExportedHandlerTailStreamHandler<Env = unknown, Props = unknown> = (
   event: TailStream.TailEvent<TailStream.Onset>,
   env: Env,
   ctx: ExecutionContext<Props>,
-) => TailStream.TailEventHandlerType | Promise<TailStream.TailEventHandlerType>;
+) =>
+  | TailStream.TailEventHandlerType
+  | undefined
+  | Promise<TailStream.TailEventHandlerType | undefined>;
 export type ExportedHandlerScheduledHandler<Env = unknown, Props = unknown> = (
   controller: ScheduledController,
   env: Env,
@@ -16150,7 +16153,8 @@ export declare namespace CloudflareWorkersModule {
       event: TailStream.TailEvent<TailStream.Onset>,
     ):
       | TailStream.TailEventHandlerType
-      | Promise<TailStream.TailEventHandlerType>;
+      | undefined
+      | Promise<TailStream.TailEventHandlerType | undefined>;
     test?(controller: TestController): void | Promise<void>;
     trace?(traces: TraceItem[]): void | Promise<void>;
   }
