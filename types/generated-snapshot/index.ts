ea50171cdd4b5ea936f026d1f182aa9442810e00
Gregory Anders <ganders@cloudflare.com>
2026-09-21 14:49:36 -0500

    Make snapshotContainer options optional (#7453)
    This was the original intent for the API, we just missed it in the
    original implementation.

--- types/generated-snapshot/index.ts
+++ types/generated-snapshot/index.ts
@@ -4002,7 +4002,7 @@ export interface Container {
   interceptOutboundHttp(addr: string, binding: Fetcher): Promise<void>;
   interceptAllOutboundHttp(binding: Fetcher): Promise<void>;
   snapshotContainer(
-    options: ContainerSnapshotOptions,
+    options?: ContainerSnapshotOptions,
   ): Promise<ContainerSnapshot>;
   interceptOutboundHttps(addr: string, binding: Fetcher): Promise<void>;
   exec(cmd: string[], options?: ContainerExecOptions): Promise<ExecProcess>;
